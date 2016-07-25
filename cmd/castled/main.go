e092653e662266261005bb370273510873099aa2
Bassam Tabbara <bassam@symform.com>
2016-07-24 22:38:40 -0700

    Fixed fmt issues

--- cmd/castled/main.go
+++ cmd/castled/main.go
@@ -6,29 +6,29 @@ package main
 import "C"
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/quantum/castle/version"
-    "github.com/spf13/cobra"
+	"github.com/quantum/castle/version"
+	"github.com/spf13/cobra"
 )
 
-func CephVersion() (string) {
-    var c_major, c_minor, c_patch C.int
-    return C.GoString(C.ceph_version(&c_major,&c_minor,&c_patch))
+func cephVersion() string {
+	var cMajor, cMinor, cPatch C.int
+	return C.GoString(C.ceph_version(&cMajor, &cMinor, &cPatch))
 }
 
 func main() {
 
-    var cmdVersion = &cobra.Command{
-        Use:   "version",
-        Short: "print castle version",
-        Long: `prints the version of castle.`,
-        Run: func(cmd *cobra.Command, args []string) {
-            fmt.Printf("castled version %v (ceph version %v)\n", version.Version, CephVersion())
-        },
-    }
+	var cmdVersion = &cobra.Command{
+		Use:   "version",
+		Short: "print castle version",
+		Long:  `prints the version of castle.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("castled version %v (ceph version %v)\n", version.Version, cephVersion())
+		},
+	}
 
-    var rootCmd = &cobra.Command{Use: "castled"}
-    rootCmd.AddCommand(cmdVersion)
-    rootCmd.Execute()
+	var rootCmd = &cobra.Command{Use: "castled"}
+	rootCmd.AddCommand(cmdVersion)
+	rootCmd.Execute()
 }
