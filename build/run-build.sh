52c296684d907f314aa2d9cba889485875a91596
Bassam Tabbara <bassam@symform.com>
2016-09-11 20:24:40 -0700

    Performance improvements to circleci build

--- build/run-build.sh
+++ build/run-build.sh
@@ -2,7 +2,6 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
 # git version 2.6.6+ through 2.8.3 had a bug with submodules
 # see https://github.com/git/git/blob/master/Documentation/RelNotes/2.8.3.txt#L33
 function ver { 
@@ -10,11 +9,12 @@ function ver {
 }
 gitversion=$(git --version | cut -d" " -f3)
 if (( $(ver ${gitversion}) > $(ver 2.6.6) && $(ver ${gitversion}) < $(ver 2.8.3) )); then
-    echo WARN: your running git version ${gitversion} which has a realted to relative
+    echo WARN: your running git version ${gitversion} which has a bug realted to relative
     echo WARN: submodule paths. Please consider upgrading to 2.8.3 or later
 fi
 
 docker run \
+    -it \
     -v ${HOME}/.netrc:/root/.netrc \
     -v ${scriptdir}/..:/go/src/github.com/quantum/castle \
     quantum/castle-build \
