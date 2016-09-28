60b0dc3b05b4531f64249ab122a59df4e8cb3cb2
Bassam Tabbara <bassam.tabbara@quantum.com>
2016-09-28 16:15:42 -0700

    Dependency checking in Makefile and other improvements
    Makefile now checks go dependencies and only builds what is needed.
    
    Refactored the Makefile into three parts for better maintenance.
    
    Add a buildall target that is the initial attempt at cross compiling
    castlectl.

--- build/publish-build-container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $scriptdir/build-container
-docker build -t quantum/castle-build .
-docker push quantum/castle-build
