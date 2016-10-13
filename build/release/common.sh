c89025c913ff2d1260f7deadc044c84327e25b0a
Bassam Tabbara <bassam.tabbara@quantum.com>
2016-10-12 20:58:34 -0700

    upload container to quay.io
    upload our containers to quay.io where they are automatically
    converted to ACIs for use by rkt. Partially addresses #50
    
    remove github-release as it was being flaky.

--- build/release/common.sh
+++ build/release/common.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-ghargs="-u ${GITHUB_USER} -r ${GITHUB_REPO} -t ${RELEASE_VERSION}"
-
 get_bindir() {
     local os=$1
     local arch=$2
@@ -29,15 +27,30 @@ layout_root() {
     cp /etc/ssl/certs/ca-certificates.crt $dir/root/etc/ssl/certs
 }
 
+github_get_release_id() {
+    curl -4 -s -H "Authorization: token ${GITHUB_TOKEN}" \
+       "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/tags/${RELEASE_VERSION}" | jq -r '.id'
+}
+
 github_check_release() {
-    if ! github-release info ${ghargs} > /dev/null 2>&1; then
-        echo "ERROR: github release tag ${version} was not found. Did you create the release?" 1>&2
+    id=$(github_get_release_id)
+
+    if [[ ${id} == "null" ]]; then
+        echo "ERROR: github release tag ${RELEASE_VERSION} was not found. Did you create the release?" 1>&2
         return 1;
     fi
 }
 
 github_upload() {
-    local file=$1
-    github-release upload ${ghargs} -n $(basename $file) -f $file
-}
+    local filepath=$1
+    local mediatype=$2
+    local filename=$(basename $filepath)
 
+    id=$(github_get_release_id)
+
+    curl -4 -s -H "Authorization: token ${GITHUB_TOKEN}" \
+         -H "Accept: application/vnd.github.manifold-preview" \
+         -H "Content-Type: application/${mediatype}" \
+         --data-binary @${filepath} \
+         "https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/${id}/assets?name=${filename}"
+}
