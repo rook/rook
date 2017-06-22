89d2272a1fd1829d6d0e37e0f12091db2ad4d781
Bassam Tabbara <bassam.tabbara@quantum.com>
2017-06-22 15:11:42 -0700

    build: remove debug binaries
    also remove rookd from tarballs

--- build/release/binaries.sh
+++ build/release/binaries.sh
@@ -43,8 +43,6 @@ build() {
     local files=( rookctl${ext} )
 
     if [[ ${os} == "linux" ]]; then
-        files+=( rookd${ext} )
-
         local tarfile=$(get_archive_name $os $arch)
         echo creating tar ${tarfile}
         tar czf "${RELEASE_DIR}/${tarfile}" -C "${bindir}" ${files[*]}
@@ -63,11 +61,6 @@ publish() {
 
     s3_upload ${RELEASE_DIR}/$file
 
-    if [[ ${os} == "linux" ]]; then
-        file=$(get_archive_name $os $arch "debug")
-        s3_upload ${RELEASE_DIR}/$file
-    fi
-
     # we will always tag master builds as latest. i.e. auto-promote master
     if [[ "${RELEASE_CHANNEL}" == "master" ]]; then
         s3_promote_release
@@ -83,14 +76,6 @@ promote() {
         s3_download ${RELEASE_DIR}/$file
     fi
     github_upload ${RELEASE_DIR}/$file
-
-    if [[ ${os} == "linux" ]]; then
-        file=$(get_archive_name $os $arch "debug")
-        if [[ ! -e ${RELEASE_DIR}/$file ]]; then
-            s3_download ${RELEASE_DIR}/$file
-        fi
-        github_upload ${RELEASE_DIR}/$file
-    fi
 }
 
 cleanup() {
