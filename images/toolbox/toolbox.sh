46d1483468a535853608c6a902aa5c322b0d5663
Travis Nielsen <travisn@live.com>
2017-10-27 11:00:36 -0700

    tools: remove dependence on api service to launch toolbox

--- images/toolbox/toolbox.sh
+++ images/toolbox/toolbox.sh
@@ -21,50 +21,64 @@ fi
 
 ROOK_DIR="/var/lib/rook"
 CEPH_CONFIG="/etc/ceph/ceph.conf"
+MON_CONFIG="/etc/rook/mon-endpoints"
+KEYRING_FILE="/etc/ceph/keyring"
 
-# attempt to create a ceph config file in its default location so ceph/rados tools can be used
+# create a ceph config file in its default location so ceph/rados tools can be used
 # without specifying any arguments
-if [[ -d ${ROOK_DIR} ]]; then
-    # there is a rook directory, try to find one of rook's ceph config files and copy it
-    ROOK_CONFIG=$(find ${ROOK_DIR} -regex '.*mon[0-9]+/.*\.config' | head -1)
-    if [[ ! -z ${ROOK_CONFIG} ]]; then
-        cp ${ROOK_CONFIG} ${CEPH_CONFIG}
-    fi
-else
-    if [[ -z ${ROOK_API_SERVER_ENDPOINT} ]]; then
-        if [[ ! -z ${ROOK_API_SERVICE_HOST} ]] && [[ ! -z ${ROOK_API_SERVICE_PORT} ]]; then
-            # we have some rook env vars set, get client info from the rook API server and construct
-            # a ceph config file from that
-            ROOK_API_SERVER_ENDPOINT=${ROOK_API_SERVICE_HOST}:${ROOK_API_SERVICE_PORT}
-        else
-            # default to the dns name of the rook-api svc
-            ROOK_API_SERVER_ENDPOINT="rook-api:8124"
-        fi
-    fi
-
-    # append to the bashrc file so that the rook tool can find the API server easily
-    cat <<EOF >> ~/.bashrc
-
-export ROOK_API_SERVER_ENDPOINT=${ROOK_API_SERVER_ENDPOINT}
-
-EOF
-
-    CLIENT_INFO=$(curl -s http://${ROOK_API_SERVER_ENDPOINT}/client)
-    MON_ENDPOINTS=$(echo $CLIENT_INFO | jq -r '.monAddresses[]' | awk -F '/' 'BEGIN { ORS="," }; {print $1}' | sed 's/,$//')
-    KEYRING_FILE="/etc/ceph/keyring"
-
+write_endpoints() {  
+    endpoints=$(cat ${MON_CONFIG})
+    mon_endpoints=$(echo ${endpoints} | sed 's/rook-ceph-mon[0-9]=//g')
+    DATE=$(date)
+    echo "$DATE writing mon endpoints to ${CEPH_CONFIG}: ${endpoints}"
     cat <<EOF > ${CEPH_CONFIG}
 [global]
-mon_host = ${MON_ENDPOINTS}
+mon_host = ${mon_endpoints}
 
 [client.admin]
 keyring = ${KEYRING_FILE}
 EOF
+}
 
-    cat <<EOF > ${KEYRING_FILE}
+# watch the endpoints config file and update if the mon endpoints ever change
+watch_endpoints() {
+    # get the timestamp for the target of the soft link
+    real_path=$(realpath ${MON_CONFIG})
+    initial_time=$(stat -c %Z ${real_path})
+    while true; do
+       real_path=$(realpath ${MON_CONFIG})
+       latest_time=$(stat -c %Z ${real_path})
+
+       if [[ "${latest_time}" != "${initial_time}" ]]; then
+         write_endpoints
+         initial_time=${latest_time}
+       fi
+       sleep 10
+    done
+}
+
+if [[ -z ${ROOK_API_SERVER_ENDPOINT} ]]; then
+    if [[ ! -z ${ROOK_API_SERVICE_HOST} ]] && [[ ! -z ${ROOK_API_SERVICE_PORT} ]]; then
+        # we have some rook env vars set, get client info from the rook API server and construct
+        # a ceph config file from that
+        ROOK_API_SERVER_ENDPOINT=${ROOK_API_SERVICE_HOST}:${ROOK_API_SERVICE_PORT}
+    fi
+fi
+
+# append to the bashrc file so that the rook tool can find the API server easily
+cat <<EOF >> ~/.bashrc
+
+export ROOK_API_SERVER_ENDPOINT=${ROOK_API_SERVER_ENDPOINT}
+EOF
+
+# create the keyring file
+cat <<EOF > ${KEYRING_FILE}
 [client.admin]
 key = ${ROOK_ADMIN_SECRET}
 EOF
-fi
 
-exec ${ARGS}
+# write the initial config file
+write_endpoints
+
+# continuously update the mon endpoints if they fail over
+watch_endpoints
