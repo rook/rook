2a1636d1ec5d3fa524de70a833527074c3898858
travisn <tnielsen@redhat.com>
2018-03-16 08:32:32 -0700

    remove the ceph-exporter that was replaced by the ceph module
    Signed-off-by: travisn <tnielsen@redhat.com>

--- pkg/daemon/ceph/model/pool.go
+++ pkg/daemon/ceph/model/pool.go
@@ -12,9 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-Some of the code below came from https://github.com/digitalocean/ceph_exporter
-which has the same license.
 */
 package model
 
