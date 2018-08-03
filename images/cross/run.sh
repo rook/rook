374125461265257911f3bfb03da5ed9759ae1dca
Bassam Tabbara <bassam@upbound.io>
2018-08-02 23:51:05 -0700

    fix perms on docker.sock
    Signed-off-by: Bassam Tabbara <bassam@upbound.io>

--- images/cross/run.sh
+++ images/cross/run.sh
@@ -29,6 +29,6 @@ useradd -o -m -g $BUILDER_GID -u $BUILDER_UID $BUILDER_USER 2> /dev/null
 echo "$BUILDER_USER    ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 export HOME=/home/${BUILDER_USER}
 echo "127.0.0.1 $(cat /etc/hostname)" >> /etc/hosts
-[[ -S /var/run/docker.sock ]] && chown $BUILDER_UID:$BUILDER_GID /var/run/docker.sock
+[[ -S /var/run/docker.sock ]] && chmod 666 /var/run/docker.sock
 chown -R $BUILDER_UID:$BUILDER_GID $HOME
 exec chpst -u :$BUILDER_UID:$BUILDER_GID ${ARGS}
