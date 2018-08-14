41ab581b76f022186763ca0e7915480df947b977
rohan47 <rohanrgupta1996@gmail.com>
2018-08-14 20:20:55 +0000

    nfs: operator/controller implementation
    Signed-off-by: rohan47 <rohanrgupta1996@gmail.com>

---
+++ cmd/rook/nfs/operator.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2018 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package nfs
+
+import (
+	"fmt"
+
+	"github.com/rook/rook/cmd/rook/rook"
+	"github.com/rook/rook/pkg/clusterd"
+	"github.com/rook/rook/pkg/operator/k8sutil"
+	operator "github.com/rook/rook/pkg/operator/nfs"
+	"github.com/rook/rook/pkg/util/flags"
+	"github.com/spf13/cobra"
+)
+
+const containerName = "rook-nfs-operator"
+
+var operatorCmd = &cobra.Command{
+	Use:   "operator",
+	Short: "Runs the NFS operator to deploy and manage NFS server in kubernetes clusters",
+	Long: `Runs the NFS operator to deploy and manage NFS server in kubernetes clusters.
+https://github.com/rook/rook`,
+}
+
+func init() {
+	flags.SetFlagsFromEnv(operatorCmd.Flags(), rook.RookEnvVarPrefix)
+
+	operatorCmd.RunE = startOperator
+}
+
+func startOperator(cmd *cobra.Command, args []string) error {
+	rook.SetLogLevel()
+	rook.LogStartupInfo(operatorCmd.Flags())
+
+	clientset, apiExtClientset, rookClientset, err := rook.GetClientset()
+	if err != nil {
+		rook.TerminateFatal(fmt.Errorf("failed to get k8s clients. %+v", err))
+	}
+
+	logger.Infof("starting NFS operator")
+	context := createContext()
+	context.NetworkInfo = clusterd.NetworkInfo{}
+	context.ConfigDir = k8sutil.DataDir
+	context.Clientset = clientset
+	context.APIExtensionClientset = apiExtClientset
+	context.RookClientset = rookClientset
+
+	// Using the current image version to deploy other rook pods
+	pod, err := k8sutil.GetRunningPod(clientset)
+	if err != nil {
+		rook.TerminateFatal(fmt.Errorf("failed to get pod. %+v\n", err))
+	}
+
+	rookImage, err := k8sutil.GetContainerImage(pod, containerName)
+	if err != nil {
+		rook.TerminateFatal(fmt.Errorf("failed to get container image. %+v\n", err))
+	}
+
+	op := operator.New(context, rookImage)
+	err = op.Run()
+	if err != nil {
+		rook.TerminateFatal(fmt.Errorf("failed to run operator. %+v\n", err))
+	}
+
+	return nil
+}
