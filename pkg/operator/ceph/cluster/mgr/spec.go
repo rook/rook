rook/rook

commit ca71b7a311e43720d31c3d1e1ef8618e7c414463

Ceph mgr: Set up config in init container
Progress toward issue #2003.
Includes design from design doc PR #1578

Use init containers to create configuration for Ceph mgrs. There is only
1 init container in this design:
 1. Using the Rook image, call the Rook binary to create Ceph config
files shared with the mgr daemgr container.

Once this init is run, the main mgr daemgr is run. Leaving room to use
the Ceph-versioned image in the future, call `ceph-mgr --foreground ...`
to run the Ceph mgr.

Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

pkg/operator/ceph/cluster/mgr/spec.go
@@ -1,0 +1,149 @@
+/*
+Copyright 2018 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mgr
+
+import (
+	"fmt"
+	"strconv"
+
+	mgrdaemon "github.com/rook/rook/pkg/daemon/ceph/mgr"
+	opmon "github.com/rook/rook/pkg/operator/ceph/cluster/mon"
+	opspec "github.com/rook/rook/pkg/operator/ceph/spec"
+	"github.com/rook/rook/pkg/operator/k8sutil"
+	"k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	mgrDaemonCommand = "ceph-mgr"
+)
+
+func (c *Cluster) makeDeployment(mgrConfig *mgrConfig) *extensions.Deployment {
+	podSpec := v1.PodTemplateSpec{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   mgrConfig.ResourceName,
+			Labels: c.getDaemonLabels(mgrConfig.DaemonName),
+			Annotations: map[string]string{"prometheus.io/scrape": "true",
+				"prometheus.io/port": strconv.Itoa(metricsPort)},
+		},
+		Spec: v1.PodSpec{
+			InitContainers: []v1.Container{
+				// Config file init performed by Rook
+				c.makeConfigInitContainer(mgrConfig),
+			},
+			Containers: []v1.Container{
+				c.makeMgrDaemonContainer(mgrConfig),
+			},
+			RestartPolicy: v1.RestartPolicyAlways,
+			Volumes:       opspec.PodVolumes(""),
+			HostNetwork:   c.HostNetwork,
+		},
+	}
+	if c.HostNetwork {
+		podSpec.Spec.DNSPolicy = v1.DNSClusterFirstWithHostNet
+	}
+	c.placement.ApplyToPodSpec(&podSpec.Spec)
+
+	replicas := int32(1)
+	d := &extensions.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      mgrConfig.ResourceName,
+			Namespace: c.Namespace,
+		},
+		Spec: extensions.DeploymentSpec{Template: podSpec, Replicas: &replicas},
+	}
+	k8sutil.SetOwnerRef(c.context.Clientset, c.Namespace, &d.ObjectMeta, &c.ownerRef)
+	return d
+}
+
+func (c *Cluster) makeConfigInitContainer(mgrConfig *mgrConfig) v1.Container {
+	return v1.Container{
+		Name: opspec.ConfigInitContainerName,
+		Args: []string{
+			"ceph",
+			mgrdaemon.InitCommand,
+			fmt.Sprintf("--config-dir=%s", k8sutil.DataDir),
+			fmt.Sprintf("--mgr-name=%s", mgrConfig.DaemonName),
+		},
+		Image: k8sutil.MakeRookImage(c.Version),
+		Env: []v1.EnvVar{
+			{Name: "ROOK_MGR_KEYRING",
+				ValueFrom: &v1.EnvVarSource{
+					SecretKeyRef: &v1.SecretKeySelector{
+						LocalObjectReference: v1.LocalObjectReference{Name: mgrConfig.ResourceName},
+						Key:                  keyringName,
+					}}},
+			k8sutil.PodIPEnvVar(k8sutil.PrivateIPEnvVar),
+			k8sutil.PodIPEnvVar(k8sutil.PublicIPEnvVar),
+			opmon.ClusterNameEnvVar(c.Namespace),
+			opmon.EndpointEnvVar(),
+			opmon.SecretEnvVar(),
+			opmon.AdminSecretEnvVar(),
+			k8sutil.ConfigOverrideEnvVar(),
+		},
+		VolumeMounts: opspec.RookVolumeMounts(),
+		// config file creation does not require ports to be open
+		Resources: c.resources,
+	}
+}
+
+func (c *Cluster) makeMgrDaemonContainer(mgrConfig *mgrConfig) v1.Container {
+	return v1.Container{
+		Name: "mgr",
+		Command: []string{
+			mgrDaemonCommand,
+		},
+		Args: []string{
+			"--foreground",
+			"--id", mgrConfig.DaemonName,
+		},
+		Image:        k8sutil.MakeRookImage(c.Version),
+		VolumeMounts: opspec.CephVolumeMounts(),
+		Ports: []v1.ContainerPort{
+			{
+				Name:          "mgr",
+				ContainerPort: int32(6800),
+				Protocol:      v1.ProtocolTCP,
+			},
+			{
+				Name:          "http-metrics",
+				ContainerPort: int32(metricsPort),
+				Protocol:      v1.ProtocolTCP,
+			},
+			{
+				Name:          "dashboard",
+				ContainerPort: int32(dashboardPort),
+				Protocol:      v1.ProtocolTCP,
+			},
+		},
+		Resources: c.resources,
+	}
+}
+
+func (c *Cluster) getLabels() map[string]string {
+	return map[string]string{
+		k8sutil.AppAttr:     appName,
+		k8sutil.ClusterAttr: c.Namespace,
+	}
+}
+
+func (c *Cluster) getDaemonLabels(daemonName string) map[string]string {
+	labels := c.getLabels()
+	labels["instance"] = daemonName
+	return labels
+}

pkg/operator/ceph/cluster/mgr/spec_test.go
@@ -1,0 +1,178 @@
+/*
+Copyright 2018 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mgr
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	cephv1beta1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1beta1"
+	rookalpha "github.com/rook/rook/pkg/apis/rook.io/v1alpha2"
+	"github.com/rook/rook/pkg/clusterd"
+	cephconfig "github.com/rook/rook/pkg/daemon/ceph/config"
+	mgrdaemon "github.com/rook/rook/pkg/daemon/ceph/mgr"
+	cephtest "github.com/rook/rook/pkg/operator/ceph/test"
+	"github.com/rook/rook/pkg/operator/k8sutil"
+	optest "github.com/rook/rook/pkg/operator/test"
+	testop "github.com/rook/rook/pkg/operator/test"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestPodSpec(t *testing.T) {
+	c := New(
+		&clusterd.Context{Clientset: testop.New(1)},
+		"ns",
+		"rook/rook:myversion",
+		rookalpha.Placement{},
+		false,
+		cephv1beta1.DashboardSpec{},
+		v1.ResourceRequirements{
+			Limits: v1.ResourceList{
+				v1.ResourceCPU: *resource.NewQuantity(100.0, resource.BinarySI),
+			},
+			Requests: v1.ResourceList{
+				v1.ResourceMemory: *resource.NewQuantity(1337.0, resource.BinarySI),
+			},
+		},
+		metav1.OwnerReference{},
+	)
+
+	mgrTestConfig := mgrConfig{
+		DaemonName:   "a",
+		ResourceName: "mgr-a",
+	}
+
+	d := c.makeDeployment(&mgrTestConfig)
+
+	assert.NotNil(t, d)
+	assert.Equal(t, "mgr-a", d.Name)
+	assert.Equal(t, "mgr-a", d.ObjectMeta.Name)
+	assert.Equal(t, 0, len(d.ObjectMeta.Annotations))
+
+	pod := d.Spec.Template
+	assert.Equal(t, appName, pod.ObjectMeta.Labels["app"])
+	assert.Equal(t, c.Namespace, pod.ObjectMeta.Labels["rook_cluster"])
+	assert.Equal(t, 2, len(pod.ObjectMeta.Annotations))
+	assert.Equal(t, "true", pod.ObjectMeta.Annotations["prometheus.io/scrape"])
+	assert.Equal(t, strconv.Itoa(metricsPort), pod.ObjectMeta.Annotations["prometheus.io/port"])
+	assert.Equal(t, v1.RestartPolicyAlways, pod.Spec.RestartPolicy)
+	assert.Nil(t, optest.VolumeExists("rook-data", pod.Spec.Volumes))
+	assert.Nil(t, optest.VolumeExists(cephconfig.DefaultConfigMountName, pod.Spec.Volumes))
+	assert.Nil(t, optest.VolumeExists(k8sutil.ConfigOverrideName, pod.Spec.Volumes))
+
+	assert.Equal(t, 1, len(pod.Spec.InitContainers))
+	assert.Equal(t, 1, len(pod.Spec.Containers))
+
+	configImage := "rook/rook:myversion"
+	configEnvs := 8
+	configContainerDefinition := cephtest.ContainerTestDefinition{
+		Image:   &configImage,
+		Command: []string{}, // no command
+		Args: [][]string{
+			{"ceph"},
+			{mgrdaemon.InitCommand},
+			{"--config-dir=/var/lib/rook"},
+			{fmt.Sprintf("--mgr-name=%s", mgrTestConfig.DaemonName)}},
+		InOrderArgs: map[int]string{
+			0: "ceph",                 // ceph must be first arg
+			1: mgrdaemon.InitCommand}, // mgr init command must be second arg
+		VolumeMountNames: []string{
+			"rook-data",
+			cephconfig.DefaultConfigMountName,
+			k8sutil.ConfigOverrideName},
+		EnvCount:     &configEnvs,
+		Ports:        []v1.ContainerPort{},
+		IsPrivileged: nil, // not set in spec
+	}
+	cont := &pod.Spec.InitContainers[0]
+	configContainerDefinition.TestContainer(t, "config init", cont, logger)
+	assert.Equal(t, "100", cont.Resources.Limits.Cpu().String())
+	assert.Equal(t, "1337", cont.Resources.Requests.Memory().String())
+
+	daemonImage := "rook/rook:myversion"
+	daemonEnvs := 0
+	daemonContainerDefinition := cephtest.ContainerTestDefinition{
+		Image: &daemonImage,
+		Command: []string{
+			"ceph-mgr"},
+		Args: [][]string{
+			{"--foreground"},
+			{"--id", mgrTestConfig.DaemonName}},
+		VolumeMountNames: []string{
+			"rook-data",
+			cephconfig.DefaultConfigMountName},
+		EnvCount: &daemonEnvs,
+		Ports: []v1.ContainerPort{
+			{ContainerPort: int32(6800),
+				Protocol: v1.ProtocolTCP},
+			{ContainerPort: int32(metricsPort),
+				Protocol: v1.ProtocolTCP},
+			{ContainerPort: int32(dashboardPort),
+				Protocol: v1.ProtocolTCP}},
+		IsPrivileged: nil, // not set in spec
+	}
+	cont = &pod.Spec.Containers[0]
+	daemonContainerDefinition.TestContainer(t, "main mon daemon", cont, logger)
+	assert.Equal(t, "100", cont.Resources.Limits.Cpu().String())
+	assert.Equal(t, "1337", cont.Resources.Requests.Memory().String())
+
+	// Verify that all the mounts have volumes and that there are no extraneous volumes
+	volsMountsTestDef := optest.VolumesAndMountsTestDefinition{
+		VolumesSpec: &optest.VolumesSpec{Moniker: "mon pod volumes", Volumes: pod.Spec.Volumes},
+		MountsSpecItems: []*optest.MountsSpec{
+			{Moniker: "mgr config init mounts", Mounts: pod.Spec.InitContainers[0].VolumeMounts},
+			{Moniker: "mgr daemon mounts", Mounts: pod.Spec.Containers[0].VolumeMounts}},
+	}
+	volsMountsTestDef.TestMountsMatchVolumes(t)
+}
+
+func TestServiceSpec(t *testing.T) {
+	c := New(&clusterd.Context{}, "ns", "myversion", rookalpha.Placement{}, false, cephv1beta1.DashboardSpec{}, v1.ResourceRequirements{}, metav1.OwnerReference{})
+
+	s := c.makeMetricsService("rook-mgr")
+	assert.NotNil(t, s)
+	assert.Equal(t, "rook-mgr", s.Name)
+	assert.Equal(t, 1, len(s.Spec.Ports))
+}
+
+func TestHostNetwork(t *testing.T) {
+	c := New(
+		&clusterd.Context{Clientset: testop.New(1)},
+		"ns",
+		"myversion",
+		rookalpha.Placement{},
+		true,
+		cephv1beta1.DashboardSpec{},
+		v1.ResourceRequirements{},
+		metav1.OwnerReference{},
+	)
+
+	mgrTestConfig := mgrConfig{
+		DaemonName:   "a",
+		ResourceName: "mgr-a",
+	}
+
+	d := c.makeDeployment(&mgrTestConfig)
+	assert.NotNil(t, d)
+
+	assert.Equal(t, true, d.Spec.Template.Spec.HostNetwork)
+	assert.Equal(t, v1.DNSClusterFirstWithHostNet, d.Spec.Template.Spec.DNSPolicy)
+}

pkg/operator/ceph/cluster/mon/spec.go
@@ -35,7 +35,7 @@ const (
 	// Full path of command used to invoke the monmap tool
 	monmaptoolCommand = "/usr/bin/monmaptool"
 	// Full path of the command used to invoke the Ceph mon daemon
-	cephMonCommand = "/usr/bin/ceph-mon"
+	cephMonCommand = "ceph-mon"
 
 	monmapFile = "monmap"
 )
@@ -82,14 +82,14 @@ func (c *Cluster) makeMonPod(monConfig *monConfig, hostname string) *v1.Pod {
 	podSpec := v1.PodSpec{
 		InitContainers: []v1.Container{
 			// Config file init performed by Rook
-			c.makeConfigInitContainer(monConfig, opspec.ConfigInitContainerName),
+			c.makeConfigInitContainer(monConfig),
 			// Ceph monmap init performed by 'monmaptool'
-			c.makeMonmapInitContainer(monConfig, "monmap-init"),
+			c.makeMonmapInitContainer(monConfig),
 			// mon filesystem init performed by mon daemon
-			c.makeMonFSInitContainer(monConfig, "mon-fs-init"),
+			c.makeMonFSInitContainer(monConfig),
 		},
 		Containers: []v1.Container{
-			c.makeMonDaemonContainer(monConfig, "mon"),
+			c.makeMonDaemonContainer(monConfig),
 		},
 		RestartPolicy: v1.RestartPolicyAlways,
 		NodeSelector:  map[string]string{apis.LabelHostname: hostname},
@@ -130,9 +130,9 @@ func podSecurityContext() *v1.SecurityContext {
 	return &v1.SecurityContext{Privileged: &privileged}
 }
 
-func (c *Cluster) makeConfigInitContainer(monConfig *monConfig, containerName string) v1.Container {
+func (c *Cluster) makeConfigInitContainer(monConfig *monConfig) v1.Container {
 	return v1.Container{
-		Name: containerName,
+		Name: opspec.ConfigInitContainerName,
 		Args: []string{
 			"ceph",
 			mondaemon.InitCommand,
@@ -164,7 +164,7 @@ func (c *Cluster) monmapFilePath(monConfig *monConfig) string {
 	)
 }
 
-func (c *Cluster) makeMonmapInitContainer(monConfig *monConfig, containerName string) v1.Container {
+func (c *Cluster) makeMonmapInitContainer(monConfig *monConfig) v1.Container {
 	// Add mons w/ monmaptool w/ args: [--add <mon-name> <mon-endpoint>]...
 	monmapAddMonArgs := []string{}
 	for _, mon := range c.clusterInfo.Monitors {
@@ -172,7 +172,7 @@ func (c *Cluster) makeMonmapInitContainer(monConfig *monConfig, containerName st
 	}
 
 	return v1.Container{
-		Name: containerName,
+		Name: "monmap-init",
 		Command: []string{
 			monmaptoolCommand,
 		},
@@ -201,9 +201,9 @@ func (c *Cluster) cephMonCommonArgs(monConfig *monConfig) []string {
 	}
 }
 
-func (c *Cluster) makeMonFSInitContainer(monConfig *monConfig, containerName string) v1.Container {
+func (c *Cluster) makeMonFSInitContainer(monConfig *monConfig) v1.Container {
 	return v1.Container{
-		Name: containerName,
+		Name: "mon-fs-init",
 		Command: []string{
 			cephMonCommand,
 		},
@@ -222,7 +222,7 @@ func (c *Cluster) makeMonFSInitContainer(monConfig *monConfig, containerName str
 	}
 }
 
-func (c *Cluster) makeMonDaemonContainer(monConfig *monConfig, containerName string) v1.Container {
+func (c *Cluster) makeMonDaemonContainer(monConfig *monConfig) v1.Container {
 	return v1.Container{
 		// The operator has set up the mon's service already, so the IP that the mon should
 		// broadcast as its own (--public-addr) is known. But the pod's IP, which the mon should
@@ -232,7 +232,7 @@ func (c *Cluster) makeMonDaemonContainer(monConfig *monConfig, containerName str
 		//    - Chosen solution, but is not as transparent to inspection as using commandline arg
 		// 2. Use bash to do variable substitution with the pod IP env var; but bash is a poor PID1
 		// 3. Use tini to do var substitution as above; but tini doesn't exist in the ceph images.
-		Name: containerName,
+		Name: "mon",
 		Command: []string{
 			cephMonCommand,
 		},

pkg/operator/ceph/cluster/mon/spec_test.go
@@ -153,7 +153,7 @@ func testPodSpec(t *testing.T, dataDir string) {
 	monFsContDev := test_opceph.ContainerTestDefinition{
 		Image: &cephImage,
 		Command: []string{
-			"/usr/bin/ceph-mon"},
+			"ceph-mon"},
 		Args: append(
 			monCommonExpectedArgs(name, c),
 			[]string{"--mkfs"},
@@ -172,7 +172,7 @@ func testPodSpec(t *testing.T, dataDir string) {
 	monDaemonContDev := test_opceph.ContainerTestDefinition{
 		Image: &cephImage,
 		Command: []string{
-			"/usr/bin/ceph-mon"},
+			"ceph-mon"},
 		Args: append(
 			monCommonExpectedArgs(name, c),
 			[]string{"--foreground"},

pkg/operator/ceph/spec/spec_test.go
@@ -24,21 +24,12 @@ import (
 )
 
 func TestPodVolumes(t *testing.T) {
-	type args struct {
-		dataDirHostPath string
+	if err := test.VolumeIsEmptyDir(k8sutil.DataDirVolume, PodVolumes("")); err != nil {
+		t.Errorf("PodVolumes(\"\") - data dir source is not EmptyDir: %s", err.Error())
 	}
-	t.Run("Empty string dataDirHostPath is EmptyDir volume", func(t *testing.T) {
-		err := test.VolumeIsEmptyDir(k8sutil.DataDirVolume, PodVolumes(""))
-		if err != nil {
-			t.Errorf("PodVolumes(\"\") - dataDirHostPath is not EmptyDir: %s", err.Error())
-		}
-	})
-	t.Run("Specified dataDirHostPath is HostPath volume", func(t *testing.T) {
-		err := test.VolumeIsHostPath(k8sutil.DataDirVolume, "/dev/sdb", PodVolumes("/dev/sdb"))
-		if err != nil {
-			t.Errorf("PodVolumes(\"/dev/sdb\") - dataDirHostPath is not HostPath: %s", err.Error())
-		}
-	})
+	if err := test.VolumeIsHostPath(k8sutil.DataDirVolume, "/dev/sdb", PodVolumes("/dev/sdb")); err != nil {
+		t.Errorf("PodVolumes(\"/dev/sdb\") - data dir source is not HostPath: %s", err.Error())
+	}
 }
 
 func TestMountsMatchVolumes(t *testing.T) {

pkg/operator/test/spec.go
@@ -1,7 +1,19 @@
-// Package testlib provides common methods for testing code which applies to many Ceph daemons.
-//
-// Methods beginning with "TestSpec" can be used to test that Kubernetes resource specs (pods, etc.)
-// are configured correctly.
+/*
+Copyright 2016 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package test
 
 import (

pkg/operator/test/spec_test.go
@@ -1,7 +1,19 @@
-// Package testlib provides common methods for testing code which applies to many Ceph daemons.
-//
-// Methods beginning with "TestSpec" can be used to test that Kubernetes resource specs (pods, etc.)
-// are configured correctly.
+/*
+Copyright 2016 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package test
 
 import "testing"

cmd/rook/ceph/mgr.go
@@ -30,23 +30,25 @@ var (
 )
 
 var mgrCmd = &cobra.Command{
-	Use:    "mgr",
-	Short:  "Generates mgr config and runs the mgr daemon",
+	Use:    mgrdaemon.InitCommand,
+	Short:  "Generates mgr config",
 	Hidden: true,
 }
 
 func init() {
-	mgrCmd.Flags().StringVar(&mgrName, "mgr-name", "", "the mgr name")
+	mgrCmd.Flags().StringVar(&mgrName, "mgr-name", "", "name of the mgr")
 	mgrCmd.Flags().StringVar(&mgrKeyring, "mgr-keyring", "", "the mgr keyring")
 	addCephFlags(mgrCmd)
 
 	flags.SetFlagsFromEnv(mgrCmd.Flags(), rook.RookEnvVarPrefix)
 
-	mgrCmd.RunE = startMgr
+	mgrCmd.RunE = initMgr
 }
 
-func startMgr(cmd *cobra.Command, args []string) error {
-	required := []string{"mon-endpoints", "cluster-name", "mon-secret", "admin-secret"}
+func initMgr(cmd *cobra.Command, args []string) error {
+	required := []string{
+		"mgr-name", "mgr-keyring",
+		"mon-endpoints", "cluster-name", "mon-secret", "admin-secret"}
 	if err := flags.VerifyRequiredFlags(mgrCmd, required); err != nil {
 		return err
 	}
@@ -66,7 +68,7 @@ func startMgr(cmd *cobra.Command, args []string) error {
 		ClusterInfo: &clusterInfo,
 	}
 
-	err := mgrdaemon.Run(createContext(), config)
+	err := mgrdaemon.Initialize(createContext(), config)
 	if err != nil {
 		rook.TerminateFatal(err)
 	}

commit 6bab32b6f90c6b6f9a7b77cfe3814d4690af8fc9

Ensure that $ROOK_CLUSTER_NAME is set for mgr daemon
We need this environment variable in the main container, not the init
container, as it's used by the rook ceph-mgr module.

Also fix up the env var counts in the testcase.

Fixes: ca71b7a311e4 (Ceph mgr: Set up config in init container)
Signed-off-by: Jeff Layton <jlayton@redhat.com>

pkg/operator/ceph/cluster/mgr/spec.go
@@ -91,7 +91,6 @@ func (c *Cluster) makeConfigInitContainer(mgrConfig *mgrConfig) v1.Container {
 					}}},
 			k8sutil.PodIPEnvVar(k8sutil.PrivateIPEnvVar),
 			k8sutil.PodIPEnvVar(k8sutil.PublicIPEnvVar),
-			opmon.ClusterNameEnvVar(c.Namespace),
 			opmon.EndpointEnvVar(),
 			opmon.SecretEnvVar(),
 			opmon.AdminSecretEnvVar(),
@@ -104,7 +103,7 @@ func (c *Cluster) makeConfigInitContainer(mgrConfig *mgrConfig) v1.Container {
 }
 
 func (c *Cluster) makeMgrDaemonContainer(mgrConfig *mgrConfig) v1.Container {
-	return v1.Container{
+	container := v1.Container{
 		Name: "mgr",
 		Command: []string{
 			mgrDaemonCommand,
@@ -136,6 +135,8 @@ func (c *Cluster) makeMgrDaemonContainer(mgrConfig *mgrConfig) v1.Container {
 		Env:       k8sutil.ClusterDaemonEnvVars(),
 		Resources: c.resources,
 	}
+	container.Env = append(container.Env, opmon.ClusterNameEnvVar(c.Namespace))
+	return container
 }
 
 func (c *Cluster) getPodLabels(daemonName string) map[string]string {

pkg/operator/ceph/cluster/mgr/spec_test.go
@@ -82,7 +82,7 @@ func TestPodSpec(t *testing.T) {
 	assert.Equal(t, 1, len(pod.Spec.Containers))
 
 	configImage := "rook/rook:myversion"
-	configEnvs := 8
+	configEnvs := 7
 	configContainerDefinition := cephtest.ContainerTestDefinition{
 		Image:   &configImage,
 		Command: []string{}, // no command
@@ -108,7 +108,8 @@ func TestPodSpec(t *testing.T) {
 	assert.Equal(t, "1337", cont.Resources.Requests.Memory().String())
 
 	daemonImage := "rook/rook:myversion"
-	daemonEnvs := len(k8sutil.ClusterDaemonEnvVars())
+	// +1 for $ROOK_CLUSTER_NAME
+	daemonEnvs := len(k8sutil.ClusterDaemonEnvVars()) + 1
 	daemonContainerDefinition := cephtest.ContainerTestDefinition{
 		Image: &daemonImage,
 		Command: []string{