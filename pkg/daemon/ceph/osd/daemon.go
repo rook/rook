41b9dfc3c10fc15ef6eecfdb75fc432ecf016fc6
Ed Schouten <ed.schouten@prodrive-technologies.com>
2019-02-25 18:57:10 +0100

    Don't automatically use filestore on systems with all devices absent.
    On setups where all specified disks are absent, we shouldn't
    automatically make use of filestore. It may well be the case that the
    disks still need to be installed.
    
    There is already code in place to realise this, except that it checks
    the list of OSDs that end up being configured, as opposed to the ones
    that are specified in configuration.
    
    Fixes: #1007
    Signed-off-by: Ed Schouten <ed.schouten@prodrive-technologies.com>

--- pkg/daemon/ceph/osd/daemon.go
+++ pkg/daemon/ceph/osd/daemon.go
@@ -132,8 +132,8 @@ func Provision(context *clusterd.Context, agent *OsdAgent) error {
 
 	// determine the set of directories that can/should be used for OSDs, with the default dir if no devices were specified. save off the node's crush name if needed.
 	logger.Infof("devices = %+v", deviceOSDs)
-	devicesConfigured := len(deviceOSDs) > 0
-	dirs, removedDirs, err := getDataDirs(context, agent.kv, agent.directories, devicesConfigured, agent.nodeName)
+	devicesSpecified := len(devices) > 0
+	dirs, removedDirs, err := getDataDirs(context, agent.kv, agent.directories, devicesSpecified, agent.nodeName)
 	if err != nil {
 		return fmt.Errorf("failed to get data dirs. %+v", err)
 	}
