3dd5aa48be2060393b6ca1effc6112ba4fb33f35
travisn <tnielsen@redhat.com>
2019-09-06 12:50:34 -0600

    ceph: Adds support for MDB controller and machineLabel controller
    Add support for MachineDisruptionBudget controller and MachineLabel controller required for fencing
    in OpenShift. This will ensure that machines are only fenced and OSDs are only stopped
    when Ceph is in a healthy state.
    
    Co-authored-by: Ashish Ranjan <aranjan@redhat.com>
    Signed-off-by: travisn <tnielsen@redhat.com>

---
+++ pkg/operator/ceph/disruption/machinedisruption/add.go
@@ -0,0 +1,60 @@
+/*
+Copyright 2019 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package machinedisruption
+
+import (
+	healthchecking "github.com/openshift/machine-api-operator/pkg/apis/healthchecking/v1alpha1"
+	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
+	"github.com/rook/rook/pkg/operator/ceph/disruption/controllerconfig"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+// Add adds a new Controller to the Manager based on machinedisruption.ReconcileMachineDisruption and registers the relevant watches and handlers.
+// Read more about how Managers, Controllers, and their Watches, Handlers, Predicates, etc work here:
+// https://godoc.org/github.com/kubernetes-sigs/controller-runtime/pkg
+func Add(mgr manager.Manager, context *controllerconfig.Context) error {
+	mgrScheme := mgr.GetScheme()
+	healthchecking.AddToScheme(mgrScheme)
+	cephv1.AddToScheme(mgrScheme)
+
+	reconcileMachineDisruption := &MachineDisruptionReconciler{
+		client:  mgr.GetClient(),
+		scheme:  mgrScheme,
+		context: context,
+	}
+
+	reconciler := reconcile.Reconciler(reconcileMachineDisruption)
+	// create a new controller
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: reconciler})
+	if err != nil {
+		return err
+	}
+
+	err = c.Watch(&source.Kind{Type: &cephv1.CephCluster{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return c.Watch(&source.Kind{Type: &healthchecking.MachineDisruptionBudget{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &cephv1.CephCluster{},
+	})
+}
---
+++ pkg/operator/ceph/disruption/machinelabel/add.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2019 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package machinelabel
+
+import (
+	mapiv1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
+	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
+	"github.com/rook/rook/pkg/operator/ceph/disruption/controllerconfig"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	osdPodLabelKey         = "app"
+	osdPODLabelValue       = "rook-ceph-osd"
+	osdClusterNameLabelKey = "rook_cluster"
+)
+
+// Add adds a new Controller to the Manager based on machinelabel.ReconcileMachineLabel and registers the relevant watches and handlers.
+// Read more about how Managers, Controllers, and their Watches, Handlers, Predicates, etc work here:
+// https://godoc.org/github.com/kubernetes-sigs/controller-runtime/pkg
+func Add(mgr manager.Manager, context *controllerconfig.Context) error {
+	mgrScheme := mgr.GetScheme()
+	cephv1.AddToScheme(mgrScheme)
+	mapiv1.AddToScheme(mgrScheme)
+
+	reconcileMachineLabel := &ReconcileMachineLabel{
+		client:  mgr.GetClient(),
+		scheme:  mgrScheme,
+		options: context,
+	}
+
+	reconciler := reconcile.Reconciler(reconcileMachineLabel)
+	// create a new controller
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: reconciler})
+	if err != nil {
+		return err
+	}
+
+	// Watch for the machines and enqueue the machineRequests if the machine is occupied by the osd pods
+	err = c.Watch(&source.Kind{Type: &mapiv1.Machine{}}, &handler.EnqueueRequestsFromMapFunc{
+		ToRequests: handler.ToRequestsFunc(func(obj handler.MapObject) []reconcile.Request {
+			clusterNamespace, isNamespacePresent := obj.Meta.GetLabels()[MachineFencingNamespaceLabelKey]
+			if !isNamespacePresent || len(clusterNamespace) == 0 {
+				return []reconcile.Request{}
+			}
+			clusterName, isClusterNamePresent := obj.Meta.GetLabels()[MachineFencingLabelKey]
+			if !isClusterNamePresent || len(clusterName) == 0 {
+				return []reconcile.Request{}
+			}
+			req := reconcile.Request{NamespacedName: types.NamespacedName{Name: clusterName, Namespace: clusterNamespace}}
+			return []reconcile.Request{req}
+		}),
+	})
+
+	// Watch for the osd pods and enqueue the CephCluster in the namespace from the pods
+	return c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestsFromMapFunc{
+		ToRequests: handler.ToRequestsFunc(func(obj handler.MapObject) []reconcile.Request {
+			_, ok := obj.Object.(*corev1.Pod)
+			if !ok {
+				return []reconcile.Request{}
+			}
+			labels := obj.Meta.GetLabels()
+			if value, present := labels[osdPodLabelKey]; !present || value != osdPODLabelValue {
+				return []reconcile.Request{}
+			}
+			namespace := obj.Meta.GetNamespace()
+			rookClusterName, present := labels[osdClusterNameLabelKey]
+			if !present {
+				return []reconcile.Request{}
+			}
+			req := reconcile.Request{NamespacedName: types.NamespacedName{Namespace: namespace, Name: rookClusterName}}
+			return []reconcile.Request{req}
+		}),
+	})
+}
