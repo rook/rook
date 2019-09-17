59d783fb5f865705d5480628a1f9cadf7af44d70
Sébastien Han <seb@redhat.com>
2019-09-17 15:08:15 +0200

    ceph: bump to v14.2.4
    v14.2.4 just got released so let's use it.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>
    (cherry picked from commit bc6dcb99956bf417f34b5f4357d7b1473a8a9291)

--- tests/framework/installer/ceph_installer.go
+++ tests/framework/installer/ceph_installer.go
@@ -41,7 +41,7 @@ const (
 	// test with the latest mimic build
 	mimicTestImage = "ceph/ceph:v13"
 	// test with the latest nautilus build
-	nautilusTestImage = "ceph/ceph:v14.2.3-20190904"
+	nautilusTestImage = "ceph/ceph:v14.2.4-20190917"
 	helmChartName     = "local/rook-ceph"
 	helmDeployName    = "rook-ceph"
 	cephOperatorLabel = "app=rook-ceph-operator"
