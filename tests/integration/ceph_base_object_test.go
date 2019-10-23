6dd474d6f4bd2a7adf8b64f311aecd590fe324bc
Umanga Chapagain <chapagainumanga@gmail.com>
2019-10-23 12:19:22 +0530

    ceph: adds retry to ObjectUser creation
    retry ObjectUser creation as long as valid input is provided
    or it times out. Retry every 15sec for 5min.
    
    integration test creates ObjectUser before ObjectStore to
    ensure that ObjectUser waits for ObjectStore to be created
    and available
    
    Closes: https://github.com/rook/rook/issues/3937
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- tests/integration/ceph_base_object_test.go
+++ tests/integration/ceph_base_object_test.go
@@ -51,13 +51,17 @@ func runObjectE2ETest(helper *clients.TestClient, k8sh *utils.K8sHelper, s suite
 	logger.Infof("Object Storage End To End Integration Test - Create Object Store, User,Bucket and read/write to bucket")
 	logger.Infof("Running on Rook Cluster %s", namespace)
 
-	logger.Infof("Step 0 : Create Object Store")
+	logger.Infof("Step 0 : Create Object Store User")
+	cosuErr := helper.ObjectUserClient.Create(namespace, userid, userdisplayname, storeName)
+
+	logger.Infof("Step 1 : Create Object Store")
 	cobsErr := helper.ObjectClient.Create(namespace, storeName, 3)
+
+	// check that ObjectStore is created
 	require.Nil(s.T(), cobsErr)
 	logger.Infof("Object store created successfully")
 
-	logger.Infof("Step 1 : Create Object Store User")
-	cosuErr := helper.ObjectUserClient.Create(namespace, userid, userdisplayname, storeName)
+	// check that ObjectUser is created
 	require.Nil(s.T(), cosuErr)
 	logger.Infof("Waiting 10 seconds to ensure user was created")
 	time.Sleep(10 * time.Second)
