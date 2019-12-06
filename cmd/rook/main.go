06c537fb61b7f4518ea01ffe0f402ca87f22f8cd
Travis Nielsen <tnielsen@redhat.com>
2019-12-06 13:43:53 -0700

    minio: remove from rook
    The minio operator has not had community support nor
    any updates since being added to Rook. Support is being
    removed from Rook due to this lack of community interest.
    
    Signed-off-by: Travis Nielsen <tnielsen@redhat.com>

--- cmd/rook/main.go
+++ cmd/rook/main.go
@@ -22,7 +22,6 @@ import (
 	"github.com/rook/rook/cmd/rook/ceph"
 	"github.com/rook/rook/cmd/rook/cockroachdb"
 	"github.com/rook/rook/cmd/rook/edgefs"
-	"github.com/rook/rook/cmd/rook/minio"
 	"github.com/rook/rook/cmd/rook/nfs"
 	rook "github.com/rook/rook/cmd/rook/rook"
 	"github.com/rook/rook/cmd/rook/util"
@@ -45,7 +44,6 @@ func addCommands() {
 		// backend commands
 		ceph.Cmd,
 		cockroachdb.Cmd,
-		minio.Cmd,
 		edgefs.Cmd,
 		nfs.Cmd,
 		cassandra.Cmd,
