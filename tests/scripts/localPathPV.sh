d5fdbcf59dcc401cda7c801cf2a521419bc27ab2
Ashish Ranjan <ashishranjan738@gmail.com>
2020-01-21 05:33:50 +0530

    ceph: Adds shell script for provisioning localPath PVs
    This commit adds a shell script to provision localPath PVs over the
    disks in the nodes for OSDs and random host directoris for mons.
    
    Note: currently it is hardcoded to select disk of `/dev/xvdb`.
    
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>

---
+++ tests/scripts/localPathPV.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8)
+
+sudo mkdir -p /var/lib/rook/${random_string}/mon1 /var/lib/rook/${random_string}/mon2 /var/lib/rook/${random_string}/mon3
+
+node_name=$(kubectl get nodes -o jsonpath={.items[*].metadata.name})
+
+kubectl label nodes ${node_name} rook.io/has-disk=true
+
+cat <<eof | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: local-vol1
+  labels:
+    type: local
+spec:
+  storageClassName: manual 
+  capacity:
+    storage: 5Gi
+  accessModes:
+    - ReadWriteOnce 
+  persistentVolumeReclaimPolicy: Retain
+  volumeMode: Filesystem
+  local:
+    path: "/var/lib/rook/${random_string}/mon1" 
+  nodeAffinity:
+      required:
+        nodeSelectorTerms:
+          - matchExpressions:
+              - key: rook.io/has-disk
+                operator: In
+                values:
+                - "true"
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: local-vol2
+  labels:
+    type: local
+spec:
+  storageClassName: manual 
+  capacity:
+    storage: 5Gi
+  accessModes:
+    - ReadWriteOnce 
+  persistentVolumeReclaimPolicy: Retain
+  volumeMode: Filesystem
+  local:
+    path: "/var/lib/rook/${random_string}/mon2" 
+  nodeAffinity:
+      required:
+        nodeSelectorTerms:
+          - matchExpressions:
+              - key: rook.io/has-disk
+                operator: In
+                values:
+                - "true"
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: local-vol3
+  labels:
+    type: local
+spec:
+  storageClassName: manual 
+  capacity:
+    storage: 5Gi
+  accessModes:
+    - ReadWriteOnce 
+  persistentVolumeReclaimPolicy: Retain
+  volumeMode: Filesystem
+  local:
+    path: "/var/lib/rook/${random_string}/mon3" 
+  nodeAffinity:
+      required:
+        nodeSelectorTerms:
+          - matchExpressions:
+              - key: rook.io/has-disk
+                operator: In
+                values:
+                - "true"
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: local-vol4
+  labels:
+    type: local
+spec:
+  storageClassName: manual 
+  capacity:
+    storage: 50Gi
+  accessModes:
+    - ReadWriteOnce 
+  persistentVolumeReclaimPolicy: Retain
+  volumeMode: Block
+  local:
+    path: "/dev/xvdb" 
+  nodeAffinity:
+      required:
+        nodeSelectorTerms:
+          - matchExpressions:
+              - key: rook.io/has-disk
+                operator: In
+                values:
+                - "true"
+---
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: manual
+provisioner: kubernetes.io/no-provisioner
+volumeBindingMode: WaitForFirstConsumer
+eof
