0c374261c79db098ce167c3d40eb5225f20c96a5
Sébastien Han <seb@redhat.com>
2020-01-24 16:47:10 +0100

    ci: re-enable multi-cluster test
    We are about to add support for external cluster in the CI so let's
    re-enable this scenario.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- tests/integration/ceph_multi_cluster_test.go
+++ tests/integration/ceph_multi_cluster_test.go
@@ -46,10 +46,9 @@ import (
 // - Create the object store via the CRD
 // *************************************************************
 func TestCephMultiClusterDeploySuite(t *testing.T) {
-	// if installer.SkipTestSuite(installer.CephTestSuite) {
-	logger.Infof("Skipping Ceph Multi Cluster Suite until we decide whether this scenario makes sense or not anymore")
-	t.Skip()
-	// }
+	if installer.SkipTestSuite(installer.CephTestSuite) {
+		t.Skip()
+	}
 
 	s := new(MultiClusterDeploySuite)
 	defer func(s *MultiClusterDeploySuite) {
