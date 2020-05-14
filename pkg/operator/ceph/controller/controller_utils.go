d1da12c6aca43449d0816f51c6480c9d28098521
Travis Nielsen <tnielsen@redhat.com>
2020-05-14 16:52:10 -0600

    ceph: wait indefinitely for cleanup before removing cluster finalizer
    During cluster deletion, we currently only retry for a couple minutes
    to wait for the pvcs to be deleted. After the timeout, we proceed
    with the cluster deletion. To properly protect the pvcs for proper
    cleanup, the finalizer should not be removed until the pvcs
    are all confirmed to be deleted. In order to not block other cluster
    events, we re-queue the deletion event to run again every 10s
    until the pvcs are deleted or the finalizer is manually removed.
    
    Signed-off-by: Travis Nielsen <tnielsen@redhat.com>

--- pkg/operator/ceph/controller/controller_utils.go
+++ pkg/operator/ceph/controller/controller_utils.go
@@ -41,6 +41,8 @@ var (
 	ImmediateRetryResult = reconcile.Result{Requeue: true}
 	// WaitForRequeueIfCephClusterNotReady waits for the CephCluster to be ready
 	WaitForRequeueIfCephClusterNotReady = reconcile.Result{Requeue: true, RequeueAfter: 10 * time.Second}
+	// WaitForRequeueIfFinalizerBlocked waits for resources to be cleaned up before the finalizer can be removed
+	WaitForRequeueIfFinalizerBlocked = reconcile.Result{Requeue: true, RequeueAfter: 10 * time.Second}
 )
 
 // IsReadyToReconcile determines if a controller is ready to reconcile or not
