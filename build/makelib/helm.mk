a86b06084988d155450557679602a5422b6e6b2c
binoue <banji-inoue@cybozu.co.jp>
2020-05-22 04:03:58 +0900

    ceph: update helm version
    Currently, Helm v2.5.1 is used, but this version is released in 2017
    and a little outdated.
    To use the latest version of helm v2.x.x would be beneficial
    as we can use more useful Helm commands like `helm template`.
    
    This commit updates Helm version and stop using `helm lint`
    with `--strict` option because Helm v2.16.7 does not allow
    to use undefined values in values.yaml and
    the Helm upstream is now stopping using the `--strict` option.
    
    Signed-off-by: binoue <banji-inoue@cybozu.co.jp>

--- build/makelib/helm.mk
+++ build/makelib/helm.mk
@@ -20,7 +20,7 @@ HELM_CHARTS_DIR ?= $(ROOT_DIR)/cluster/charts
 HELM_OUTPUT_DIR ?= $(OUTPUT_DIR)/charts
 
 HELM_HOME := $(abspath $(CACHE_DIR)/helm)
-HELM_VERSION := v2.5.1
+HELM_VERSION := v2.16.7
 HELM := $(TOOLS_HOST_DIR)/helm-$(HELM_VERSION)
 HELM_INDEX := $(HELM_OUTPUT_DIR)/index.yaml
 export HELM_HOME
@@ -43,7 +43,7 @@ $(HELM_OUTPUT_DIR)/$(1)-$(VERSION).tgz: $(HELM) $(HELM_OUTPUT_DIR) $(shell find 
 	@echo === helm package $(1)
 	@cp -r $(HELM_CHARTS_DIR)/$(1) $(OUTPUT_DIR)
 	@$(SED_CMD) 's|VERSION|$(VERSION)|g' $(OUTPUT_DIR)/$(1)/values.yaml
-	@$(HELM) lint --strict $(abspath $(OUTPUT_DIR)/$(1))
+	@$(HELM) lint $(abspath $(OUTPUT_DIR)/$(1))
 	@$(HELM) package --version $(VERSION) -d $(HELM_OUTPUT_DIR) $(abspath $(OUTPUT_DIR)/$(1))
 $(HELM_INDEX): $(HELM_OUTPUT_DIR)/$(1)-$(VERSION).tgz
 endef
