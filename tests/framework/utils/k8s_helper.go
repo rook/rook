8c8e9dcaa1153716a788992d23ed0af94f4f9c9b
binoue <banji-inoue@cybozu.co.jp>
2020-08-05 11:37:43 +0900

    ceph: fix code
    fix code
    
    Signed-off-by: binoue <banji-inoue@cybozu.co.jp>

--- tests/framework/utils/k8s_helper.go
+++ tests/framework/utils/k8s_helper.go
@@ -555,7 +555,7 @@ func (k8sh *K8sHelper) GetEventsFromNamespace(namespace, testName, platformName 
 
 	file, err := k8sh.createTestLogFile(platformName, "events", namespace, testName, "")
 	if err != nil {
-		logger.Errorf("failed to create event file. %v", err)s
+		logger.Errorf("failed to create event file. %v", err)
 		return
 	}
 	defer file.Close()
