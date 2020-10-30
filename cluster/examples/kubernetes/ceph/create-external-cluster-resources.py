7b8da65c81e16b06107b133e71a39667066e4318
Arun Kumar Mohan <amohan@redhat.com>
2020-10-31 02:17:49 +0530

    ceph: fixed existing test cases
    Modified / Fixed all the existing tests according to the latest changes.
    
    Signed-off-by: Arun Kumar Mohan <amohan@redhat.com>

--- cluster/examples/kubernetes/ceph/create-external-cluster-resources.py
+++ cluster/examples/kubernetes/ceph/create-external-cluster-resources.py
@@ -144,6 +144,10 @@ class RadosJSON:
         return False
 
     def endpoint_dial(self, endpoint_str, timeout=3):
+        # if the 'cluster' instance is a dummy one,
+        # don't try to reach out to the endpoint
+        if isinstance(self.cluster, DummyRados):
+            return
         protocols = ["http", "https"]
         for prefix in protocols:
             try:
@@ -235,6 +239,16 @@ class RadosJSON:
             return endpoint
         return ':'.join([endpoint, port])
 
+    def _convert_hostname_to_ip(self, host_name):
+        # if 'cluster' instance is a dummy type,
+        # return a sample IP
+        if isinstance(self.cluster, DummyRados):
+            return "10.10.92.110"
+        import socket
+        ip = socket.gethostbyname(host_name)
+        del socket
+        return ip
+
     def get_active_ceph_mgr(self):
         if self._arg_parser.monitoring_endpoint:
             monitoring_endpoint_ip = self._arg_parser.monitoring_endpoint
@@ -263,8 +277,7 @@ class RadosJSON:
             monitoring_endpoint_port = ''
 
         try:
-            import socket
-            monitoring_endpoint_ip = socket.gethostbyname(monitoring_endpoint_ip)
+            monitoring_endpoint_ip = self._convert_hostname_to_ip(monitoring_endpoint_ip)
         except:
             raise ExecutionFailureException(
                 "unable to convert a hostname to an IP address, monitoring host name: {}".format(monitoring_endpoint_ip))
@@ -702,6 +715,8 @@ class DummyRados(object):
         self.cmd_output_map['''{"caps": ["mon", "profile rbd", "mgr", "allow rw", "osd", "profile rbd"], "entity": "client.csi-rbd-provisioner", "format": "json", "prefix": "auth get-or-create"}'''] = '''[{"entity":"client.csi-rbd-provisioner","key":"AQBNgrNe1geyKxAA8ekViRdE+hss5OweYBkwNg==","caps":{"mgr":"allow rw","mon":"profile rbd","osd":"profile rbd"}}]'''
         self.cmd_output_map['''{"caps": ["mon", "allow r", "mgr", "allow rw", "osd", "allow rw tag cephfs *=*", "mds", "allow rw"], "entity": "client.csi-cephfs-node", "format": "json", "prefix": "auth get-or-create"}'''] = '''[{"entity":"client.csi-cephfs-node","key":"AQBOgrNeENunKxAAPCmgE7R6G8DcXnaJ1F32qg==","caps":{"mds":"allow rw","mgr":"allow rw","mon":"allow r","osd":"allow rw tag cephfs *=*"}}]'''
         self.cmd_output_map['''{"caps": ["mon", "allow r", "mgr", "allow rw", "osd", "allow rw tag cephfs metadata=*"], "entity": "client.csi-cephfs-provisioner", "format": "json", "prefix": "auth get-or-create"}'''] = '''[{"entity":"client.csi-cephfs-provisioner","key":"AQBOgrNeAFgcGBAAvGqKOAD0D3xxmVY0R912dg==","caps":{"mgr":"allow rw","mon":"allow r","osd":"allow rw tag cephfs metadata=*"}}]'''
+        self.cmd_output_map['''{"caps": ["mon", "allow r, allow command quorum_status, allow command version", "mgr", "allow command config", "osd", "allow rwx pool=default.rgw.meta, allow r pool=.rgw.root, allow rw pool=default.rgw.control, allow rx pool=default.rgw.log, allow x pool=default.rgw.buckets.index"], "entity": "client.healthchecker", "format": "json", "prefix": "auth get-or-create"}'''] = '''[{"entity":"client.healthchecker","key":"AQDFkbNeft5bFRAATndLNUSEKruozxiZi3lrdA==","caps":{"mon": "allow r, allow command quorum_status, allow command version", "mgr": "allow command config", "osd": "allow rwx pool=default.rgw.meta, allow r pool=.rgw.root, allow rw pool=default.rgw.control, allow rx pool=default.rgw.log, allow x pool=default.rgw.buckets.index"}}]'''
+        self.cmd_output_map['''{"format": "json", "prefix": "mgr services"}'''] = '''{"dashboard": "http://rook-ceph-mgr-a-57cf9f84bc-f4jnl:7000/", "prometheus": "http://rook-ceph-mgr-a-57cf9f84bc-f4jnl:9283/"}'''
 
     def shutdown(self):
         pass
@@ -714,6 +729,9 @@ class DummyRados(object):
 
     def connect(self):
         pass
+
+    def pool_exists(self, pool_name):
+        return True
 
     def mon_command(self, cmd, out):
         json_cmd = json.loads(cmd)
@@ -733,14 +751,14 @@ class DummyRados(object):
 # python -m unittest --verbose <script_name_without_dot_py>
 class TestRadosJSON(unittest.TestCase):
     def setUp(self):
-        print("{}".format("I am in setup"))
+        print("\nI am in setup")
         self.rjObj = RadosJSON(['--rbd-data-pool-name=abc',
                                 '--rgw-endpoint=10.10.212.122:9000', '--format=json'])
         # for testing, we are using 'DummyRados' object
         self.rjObj.cluster = DummyRados.Rados()
 
     def tearDown(self):
-        print("{}".format("I am tearing down the setup"))
+        print("\nI am tearing down the setup\n")
         self.rjObj.shutdown()
 
     def test_method_main_output(self):
@@ -783,9 +801,9 @@ class TestRadosJSON(unittest.TestCase):
         # but no specific '--cephfs-filesystem-name' argument provided
         try:
             self.rjObj.get_cephfs_data_pool_details()
-            self.fail("An Exception was expected to be thrown")
+            print("As we are returning silently, no error thrown as expected")
         except ExecutionFailureException as err:
-            print("Successfully thrown error: {}".format(err))
+            self.fail("Supposed to get returned silently, but instead error thrown: {}".format(err))
         # pass an existing filesystem name
         try:
             self.rjObj._arg_parser.cephfs_filesystem_name = second_fs_details['name']
