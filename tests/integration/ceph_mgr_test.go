7c3fc1671f2a34780004e873a0c86407576a59bb
subhamkrai <srai@redhat.com>
2020-11-18 20:02:32 +0530

    ceph: run TestMgrSmokeSuite in Gh
    currently, TestMgrSmokeSuite is skipped
    due to random errors. This commit
    check Mgr suite on github actions.
    
    Signed-off-by: subhamkrai <srai@redhat.com>

--- tests/integration/ceph_mgr_test.go
+++ tests/integration/ceph_mgr_test.go
@@ -44,14 +44,6 @@ func TestCephMgrSuite(t *testing.T) {
 	if installer.SkipTestSuite(installer.CephTestSuite) {
 		t.Skip()
 	}
-	// Skip this test suite in master and release builds. If there is an issue
-	// running against Ceph master we don't want to block the official builds.
-	if installer.TestIsOfficialBuild() {
-		t.Skip()
-	}
-
-	logger.Info("TEMPORARILY disable the mgr test suite until https://github.com/rook/rook/issues/5877 is resolved")
-	t.Skip()
 
 	s := new(CephMgrSuite)
 	defer func(s *CephMgrSuite) {
