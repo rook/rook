5fc66ea2ae32640d1693cac04170bf4b05d535b2
Blaine Gardner <blaine.gardner@redhat.com>
2021-02-01 12:33:52 -0700

    ceph: do not force go path in unit tests
    Do not force unit tests to be run while the rook repo is in the GOPATH.
    Now that Rook uses go modules, this is no longer necessary. Allow this
    by creating a helper function that unit tests can use that return the
    on-host path to the `rook` project root.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>
    (cherry picked from commit 15a487cd5aa5d4dc59bc15ef610cb182257d9287)

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package util
 
 import (
@@ -21,6 +22,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	"github.com/coreos/pkg/capnslog"
 )
@@ -47,4 +49,14 @@ func WriteFileToLog(logger *capnslog.PackageLogger, path string) {
 	}
 
 	logger.Infof("Config file %s:\n%s", path, string(contents))
+}
+
+// PathToProjectRoot returns the path to the root of the rook repo on the current host.
+// This is primarily useful for tests.
+func PathToProjectRoot() string {
+	_, path, _, _ := runtime.Caller(0) // get path to current file (<root>/pkg/util/file.go)
+	util := filepath.Dir(path)         // <root>/pkg/util
+	pkg := filepath.Dir(util)          // <root>/pkg
+	root := filepath.Dir(pkg)          // <root>
+	return root
 }
