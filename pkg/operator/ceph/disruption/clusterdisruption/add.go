ac75eeec0698bcf35d42abaf9b16d285ecb585f2
Santosh Pillai <sapillai@redhat.com>
2021-02-02 21:49:07 +0530

    ceph: skip pdb reconcile on create and delete events
    This PR makes create and delete events on pdb a no-op. Update event only triggers reconcile if its the main OSD pdb and allowed disruptions is 0.
    This prevents controller to reconcile on pdb events namespaces outside rook ceph.
    
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>
    (cherry picked from commit b780e5b5c052bac61d8b6498c4e68c1d103d202e)

--- pkg/operator/ceph/disruption/clusterdisruption/add.go
+++ pkg/operator/ceph/disruption/clusterdisruption/add.go
@@ -46,7 +46,7 @@ func Add(mgr manager.Manager, context *controllerconfig.Context) error {
 		return errors.Wrap(err, "failed to add ceph scheme to manager scheme")
 	}
 
-	// this will be used to associate namespaces and cephclusters.
+	// This will be used to associate namespaces and cephclusters.
 	sharedClusterMap := &ClusterMap{}
 
 	reconcileClusterDisruption := &ReconcileClusterDisruption{
@@ -86,14 +86,23 @@ func Add(mgr manager.Manager, context *controllerconfig.Context) error {
 		return err
 	}
 
+	// Only reconcile for PDB update event when allowed disruptions for the main OSD PDB is 0.
+	// This means that one of the OSD is down due to node drain or any other reason
 	pdbPredicate := predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			// Do not reconcile when PDB is created
+			return false
+		},
 		UpdateFunc: func(e event.UpdateEvent) bool {
 			pdb, ok := e.ObjectNew.DeepCopyObject().(*policyv1beta1.PodDisruptionBudget)
 			if !ok {
 				return false
 			}
-			// only reconcile if allowed disruptions is 0 in the main PDB
 			return pdb.Name == osdPDBAppName && pdb.Status.DisruptionsAllowed == 0
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			// Do not reconcile when PDB is deleted
+			return false
 		},
 	}
 
