7095db6ddfcae64a8db250696f90c998d4dae317
subhamkrai <srai@redhat.com>
2021-02-17 07:35:18 +0530

    ceph: update placement in case of osd on pvc
    this commit update placement for osd on pvc.
    Earlier it was applying storage deviceset
    prepare osd Placement(if applied or else
    spec.placement.prepareosd).
    Now it will apply storage deviceset osd placement
    (if applied or else spec.placement.osd).
    
    Signed-off-by: subhamkrai <srai@redhat.com>
    (cherry picked from commit b20a7f13ee65512c06ce7958d99ccd43768a1b43)

--- pkg/operator/ceph/cluster/osd/spec.go
+++ pkg/operator/ceph/cluster/osd/spec.go
@@ -642,7 +642,7 @@ func (c *Cluster) makeDeployment(osdProps osdProperties, osd OSDInfo, provisionC
 	// we are passing false in case of osd and prepare pods because we don't want to overlap placement of osd on PVC's and non-PVC's.
 	p := cephv1.GetOSDPlacement(c.spec.Placement)
 	if osdProps.onPVC() {
-		osdProps.getPreparePlacement().ApplyToPodSpec(&deployment.Spec.Template.Spec, false)
+		osdProps.placement.ApplyToPodSpec(&deployment.Spec.Template.Spec, false)
 	}
 	p.ApplyToPodSpec(&deployment.Spec.Template.Spec, false)
 
