497202815435c3e471b37c39b3a7c2a967efaec8
Blaine Gardner <blaine.gardner@redhat.com>
2021-02-24 15:13:22 -0700

    ceph: update build and examples to use v15.2.9
    There are still references to v15.2.7 in integration tests to allow
    testing OSDs on partitions.
    
    The newest dated Ceph image is ceph/ceph:v15.2.9-20210224
    
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>
    (cherry picked from commit 818342cbfb04c90d5082e1f8f78e03b2ef3c0565)

--- pkg/apis/ceph.rook.io/v1/types.go
+++ pkg/apis/ceph.rook.io/v1/types.go
@@ -163,7 +163,7 @@ type KeyManagementServiceSpec struct {
 
 // CephVersionSpec represents the settings for the Ceph version that Rook is orchestrating.
 type CephVersionSpec struct {
-	// Image is the container image used to launch the ceph daemons, such as ceph/ceph:v15.2.8
+	// Image is the container image used to launch the ceph daemons, such as ceph/ceph:v15.2.9
 	Image string `json:"image,omitempty"`
 
 	// Whether to allow unsupported versions (do not set to true in production)
