8b6ec606532534a0754862b5ef0a49a6949097cb
subhamkrai <srai@redhat.com>
2021-02-27 19:32:04 +0530

    ceph: use cert-manager for admission webhooks
    we are now using cert-manager for certificates
    instead of a self-signed certificate for admission
    webhooks.
    
    Signed-off-by: subhamkrai <srai@redhat.com>

--- tests/scripts/deploy_admission_controller.sh
+++ tests/scripts/deploy_admission_controller.sh
@@ -1,26 +1,60 @@
 #!/usr/bin/env bash
 # deploy_admission_controller.sh
 # Sets up the environment for the admission controller webhook in the active cluster.
-set -eo pipefail
+set -eEo pipefail
+
+function cleanup() {
+  set +e
+  kubectl -n rook-ceph delete validatingwebhookconfigurations $WEBHOOK_CONFIG_NAME
+  kubectl -n rook-ceph delete certificate rook-admission-controller-cert
+  kubectl -n rook-ceph delete issuers selfsigned-issuer
+  kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/$CERT_VERSION/cert-manager.yaml
+  set -e
+}
+trap cleanup SIGINT ERR
 
 # Set our known directories and parameters.
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+CERT_VERSION="v1.2.0"
 
-# Set the variables for webhook. Any change here for namespace and service name should be done in pkg/operator/admission/init.go also
 [ -z "${NAMESPACE}" ] && NAMESPACE="rook-ceph"
-WEBHOOK_CONFIG_NAME="rook-ceph-webhook"
-SERVICE_NAME="rook-ceph-admission-controller"
+export NAMESPACE
+export WEBHOOK_CONFIG_NAME="rook-ceph-webhook"
+export SERVICE_NAME="rook-ceph-admission-controller"
 
-INSTALL_SELF_SIGNED_CERT=true
 echo "$BASE_DIR"
 
-if [ "${INSTALL_SELF_SIGNED_CERT}" == true ]; then
-	"${BASE_DIR}"/webhook-create-signed-cert.sh --namespace ${NAMESPACE}
-fi
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/$CERT_VERSION/cert-manager.yaml
+timeout 150 sh -c 'until [ $(kubectl -n cert-manager get pods --field-selector=status.phase=Running|grep -c ^cert-) -eq 3 ]; do sleep 1; done'
+timeout 20 sh -c 'until [ $(kubectl -n cert-manager get pods -o custom-columns=READY:status.containerStatuses[*].ready | grep -c true) -eq 3 ]; do sleep 1; done'
+
 echo "Deploying webhook config"
-export NAMESPACE
-export WEBHOOK_CONFIG_NAME
-export SERVICE_NAME
+
+cat <<EOF | kubectl create -f -
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: selfsigned-issuer
+  namespace: ${NAMESPACE}
+spec:
+  selfSigned: {}
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: rook-admission-controller-cert
+  namespace: ${NAMESPACE}
+spec:
+  dnsNames:
+  - ${SERVICE_NAME}
+  - ${SERVICE_NAME}.${NAMESPACE}.svc
+  - ${SERVICE_NAME}.${NAMESPACE}.svc.cluster.local
+  issuerRef:
+    kind: Issuer
+    name: selfsigned-issuer
+  secretName: rook-ceph-admission-controller
+EOF
+
 cat ${BASE_DIR}/webhook-config.yaml | \
         "${BASE_DIR}"/webhook-patch-ca-bundle.sh | \
         sed -e "s|\${NAMESPACE}|${NAMESPACE}|g" | \
