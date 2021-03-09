b75a5f4f8c3997bdf71ad96b21dcb07237ffd2d1
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2021-03-09 05:14:54 +0000

    ceph: fix improper json parsing in radosgw-admin
    Sometimes `radosgw-admin` succeeds after showing logs to stderr. We should
    skip non-json strings if parsing output as json.
    
    Here is an example.
    
    ```
    2021-02-26 04:10:44.190418 I | op-bucket-prov: creating Ceph user "ceph-user-aSzNqgE7"
    E0226 04:11:37.901310       8 controller.go:199] error syncing 'logging/loki-bucket': error provisioning bucket: Provision: can't create ceph user: error creating ceph user "ceph-user-aSzNqgE7": failed to unmarshal json. 2021-02-26T04:11:21.425+0000 7f6714be4980  1 robust_notify: If at first you don't succeed: (110) Connection timed out
    2021-02-26T04:11:21.426+0000 7f6714be4980  0 ERROR: failed to distribute cache for ceph-hdd-object-store.rgw.meta:users.uid:ceph-user-aSzNqgE7
    2021-02-26T04:11:32.168+0000 7f6714be4980  1 robust_notify: If at first you don't succeed: (110) Connection timed out
    2021-02-26T04:11:32.168+0000 7f6714be4980  0 ERROR: failed to distribute cache for ceph-hdd-object-store.rgw.meta:users.keys:23Z8GUEXR0TJDO86PSJR
    {
        "user_id": "ceph-user-aSzNqgE7",
        "display_name": "ceph-user-aSzNqgE7",
    ...
        "mfa_ids": []
    }: invalid character '-' after top-level value: failed to unmarshal json.
    ```
    
    In this case, some logs like "robust_notify:..." was shown in stderr.
    Unmarsharing was failed due to tried to parse these logs as json.
    
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>
    (cherry picked from commit a89d35e5b6c8a7ca12ae12d497f3ce81d006760b)

---
+++ pkg/operator/ceph/object/admin_test.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2021 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package object
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractJson(t *testing.T) {
+	s := "invalid json"
+	_, err := extractJSON(s)
+	assert.Error(t, err)
+
+	s = `{"test": "test"}`
+	match, err := extractJSON(s)
+	assert.NoError(t, err)
+	assert.True(t, json.Valid([]byte(match)))
+
+	s = `this line can't be parsed as json
+{"test": "test"}`
+	match, err = extractJSON(s)
+	assert.NoError(t, err)
+	assert.True(t, json.Valid([]byte(match)))
+
+	s = `this line can't be parsed as json
+{"test":
+"test"}`
+	match, err = extractJSON(s)
+	assert.NoError(t, err)
+	assert.True(t, json.Valid([]byte(match)))
+}
