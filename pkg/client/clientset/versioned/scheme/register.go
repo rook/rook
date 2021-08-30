59cd35c277f5da8f3e45134bc4751f244974d3b9
Blaine Gardner <blaine.gardner@redhat.com>
2021-08-30 13:13:29 -0600

    ceph: remove NFS and Cassandra operator code
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>
    (cherry picked from commit a1814af1d92102c3456725dc9c704da0c289e482)

--- pkg/client/clientset/versioned/scheme/register.go
+++ pkg/client/clientset/versioned/scheme/register.go
@@ -19,9 +19,7 @@ limitations under the License.
 package scheme
 
 import (
-	cassandrav1alpha1 "github.com/rook/rook/pkg/apis/cassandra.rook.io/v1alpha1"
 	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
-	nfsv1alpha1 "github.com/rook/rook/pkg/apis/nfs.rook.io/v1alpha1"
 	rookv1alpha2 "github.com/rook/rook/pkg/apis/rook.io/v1alpha2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
@@ -34,9 +32,7 @@ var Scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(Scheme)
 var ParameterCodec = runtime.NewParameterCodec(Scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
-	cassandrav1alpha1.AddToScheme,
 	cephv1.AddToScheme,
-	nfsv1alpha1.AddToScheme,
 	rookv1alpha2.AddToScheme,
 }
 
