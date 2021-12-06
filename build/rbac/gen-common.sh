757960ba28d636dd07dd738f5f989ce58afa2843
Blaine Gardner <blaine.gardner@redhat.com>
2021-12-06 11:15:06 -0700

    build: generate common.yaml from helm charts
    Generate deploy/examples/common.yaml from Helm charts. There were still
    a few differences between common.yaml and the Helm charts. Where there
    were differences we assume common.yaml was the most up-to-date.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>
    (cherry picked from commit bcc7d3e54103dc8ef9a9e64cd107d0e4feff0e98)

---
+++ build/rbac/gen-common.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xeEuo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd "$SCRIPT_DIR" &>/dev/stderr
+
+COMMON_YAML_FILE="$SCRIPT_DIR/../../deploy/examples/common.yaml"
+
+rm -f "$COMMON_YAML_FILE"
+cat common.yaml.header >> "$COMMON_YAML_FILE"
+./get-helm-rbac.sh >> "$COMMON_YAML_FILE"
+
+popd &>/dev/stderr
