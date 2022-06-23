92a4733126b2adf9cdf48e25b58f3722484f3274
Rakshith R <rar@redhat.com>
2022-06-23 16:43:20 +0530

    ci: rename TestCephSettings.EnableCSINFS to TestNFSCSI
    Signed-off-by: Rakshith R <rar@redhat.com>
    (cherry picked from commit 928dd46593b248a7935edca37957b3e4b7c9864e)

--- tests/framework/installer/ceph_settings.go
+++ tests/framework/installer/ceph_settings.go
@@ -47,7 +47,7 @@ type TestCephSettings struct {
 	ConnectionsEncrypted        bool
 	ConnectionsCompressed       bool
 	EnableVolumeReplication     bool
-	EnableCSINFS                bool
+	TestNFSCSI                  bool
 	ChangeHostName              bool
 	RookVersion                 string
 	CephVersion                 cephv1.CephVersionSpec
@@ -88,7 +88,7 @@ func (s *TestCephSettings) replaceOperatorSettings(manifest string) string {
 	manifest = strings.ReplaceAll(manifest, `# CSI_LOG_LEVEL: "0"`, `CSI_LOG_LEVEL: "5"`)
 	manifest = strings.ReplaceAll(manifest, `ROOK_ENABLE_DISCOVERY_DAEMON: "false"`, fmt.Sprintf(`ROOK_ENABLE_DISCOVERY_DAEMON: "%t"`, s.EnableDiscovery))
 	manifest = strings.ReplaceAll(manifest, `CSI_ENABLE_VOLUME_REPLICATION: "false"`, fmt.Sprintf(`CSI_ENABLE_VOLUME_REPLICATION: "%t"`, s.EnableVolumeReplication))
-	manifest = strings.ReplaceAll(manifest, `ROOK_CSI_ENABLE_NFS: "false"`, fmt.Sprintf(`ROOK_CSI_ENABLE_NFS: "%t"`, s.EnableCSINFS))
+	manifest = strings.ReplaceAll(manifest, `ROOK_CSI_ENABLE_NFS: "false"`, fmt.Sprintf(`ROOK_CSI_ENABLE_NFS: "%t"`, s.TestNFSCSI))
 	return manifest
 }
 
