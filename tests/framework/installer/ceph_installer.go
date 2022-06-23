ab54db1b09acbc122b99ad12d3b2a3b88c3124ea
Blaine Gardner <blaine.gardner@redhat.com>
2022-06-23 15:38:58 -0600

    ci: use ceph v17.2.0 for testing
    The CI smoke suite for CephFilesystem has been flaky since the quay
    image for Ceph v17.2.1 was released. Revert to using v17.2.0.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>

--- tests/framework/installer/ceph_installer.go
+++ tests/framework/installer/ceph_installer.go
@@ -47,7 +47,7 @@ const (
 	// test with the latest pacific build
 	pacificTestImage = "quay.io/ceph/ceph:v16"
 	// test with the latest pacific build
-	quincyTestImage = "quay.io/ceph/ceph:v17"
+	quincyTestImage = "quay.io/ceph/ceph:v17.2.0"
 	// test with the current development version of Pacific
 	octopusDevelTestImage = "quay.io/ceph/daemon-base:latest-octopus-devel"
 	pacificDevelTestImage = "quay.io/ceph/daemon-base:latest-pacific-devel"
