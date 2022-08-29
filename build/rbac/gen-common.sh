cd1baf0efe8e5ffd0901a1e976a9cef673c869f3
Blaine Gardner <blaine.gardner@redhat.com>
2022-08-29 10:41:41 -0600

    build: remove psp from common.yaml generation
    Due to an oversight, PSP resources were left in generation of
    common.yaml from #10797. Resolve that by setting
    pspEnable=false when generating common.yaml.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>
    (cherry picked from commit 6c8f2e414fde3d8c8f9ef55fa5fae000a7cd00e8)

--- build/rbac/gen-common.sh
+++ build/rbac/gen-common.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -xeEuo pipefail
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 pushd "$SCRIPT_DIR" &>/dev/stderr
 
 COMMON_YAML_FILE="$SCRIPT_DIR/../../deploy/examples/common.yaml"
 
+export ADDITIONAL_HELM_CLI_OPTIONS="--set pspEnable=false"
+
 rm -f "$COMMON_YAML_FILE"
-cat common.yaml.header >> "$COMMON_YAML_FILE"
-./get-helm-rbac.sh >> "$COMMON_YAML_FILE"
+cat common.yaml.header >>"$COMMON_YAML_FILE"
+./get-helm-rbac.sh >>"$COMMON_YAML_FILE"
 
 popd &>/dev/stderr
