88f66ff1f2c73c6087338e8f6080b75ccff38713
Travis Nielsen <tnielsen@redhat.com>
2023-02-14 17:23:19 -0700

    test: fix upgrade test for msgr2 setting
    The requireMsgr2 setting is not available in 1.10, therefore
    the setting must be removed from the cephcluster CR
    when the 1.10 cluster is created before the upgrade.
    
    Signed-off-by: Travis Nielsen <tnielsen@redhat.com>

--- tests/framework/installer/ceph_manifests_previous.go
+++ tests/framework/installer/ceph_manifests_previous.go
@@ -86,7 +86,13 @@ func (m *CephManifestsPreviousVersion) GetCommonExternal() string {
 
 // GetRookCluster returns rook-cluster manifest
 func (m *CephManifestsPreviousVersion) GetCephCluster() string {
-	return m.latest.GetCephCluster()
+	c := m.latest.GetCephCluster()
+
+	// Remove the requireMsgr2 setting if in v1.10
+	if m.settings.RookVersion == Version1_10 {
+		return strings.Replace(c, "requireMsgr2: false", "", 1)
+	}
+	return c
 }
 
 func (m *CephManifestsPreviousVersion) GetBlockSnapshotClass(snapshotClassName, reclaimPolicy string) string {
