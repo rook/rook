6a221227af90d4b572f6a4d253c70092be011e92
parth-gr <paarora@redhat.com>
2023-02-17 18:37:39 +0530

    build: update golang version to v1.19
    Updated the go mod and ci to use gov1.19 as
    minimum and removed support for go1.18
    
    Closes: https://github.com/rook/rook/issues/11681
    Signed-off-by: parth-gr <paarora@redhat.com>

--- build/makelib/golang.mk
+++ build/makelib/golang.mk
@@ -48,7 +48,7 @@ GO_TEST_FLAGS ?=
 # ====================================================================================
 # Setup go environment
 
-GO_SUPPORTED_VERSIONS ?= 1.18|1.19
+GO_SUPPORTED_VERSIONS ?= 1.19|1.20
 
 GO_PACKAGES := $(foreach t,$(GO_SUBDIRS),$(GO_PROJECT)/$(t)/...)
 GO_INTEGRATION_TEST_PACKAGES := $(foreach t,$(GO_INTEGRATION_TESTS_SUBDIRS),$(GO_PROJECT)/$(t)/integration)
