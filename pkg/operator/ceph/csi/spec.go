b01180580c4d808850db2edf95fa7bd6d493a81a
Madhu Rajanna <madhupr007@gmail.com>
2023-02-22 11:05:15 +0100

    csi: update cephcsi to v3.8.0
    updating the cephcsi image to v3.8.0
    release and also removing support for
    3.6.0 as its deprecated and not supported
    any more.
    
    More details at
    https://github.com/ceph/ceph-csi/releases/tag/v3.8.0
    and https://github.com/ceph/ceph-csi/tree/release-v3.8
    \#ceph-csi-container-images-and-release-compatibility
    
    fixes #11688
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- pkg/operator/ceph/csi/spec.go
+++ pkg/operator/ceph/csi/spec.go
@@ -134,7 +134,7 @@ var (
 // manually challenging.
 var (
 	// image names
-	DefaultCSIPluginImage   = "quay.io/cephcsi/cephcsi:v3.7.2"
+	DefaultCSIPluginImage   = "quay.io/cephcsi/cephcsi:v3.8.0"
 	DefaultRegistrarImage   = "registry.k8s.io/sig-storage/csi-node-driver-registrar:v2.7.0"
 	DefaultProvisionerImage = "registry.k8s.io/sig-storage/csi-provisioner:v3.4.0"
 	DefaultAttacherImage    = "registry.k8s.io/sig-storage/csi-attacher:v4.1.0"
