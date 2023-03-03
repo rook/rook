580a9ec5c3495b462b54d40e5c36f0b0c0becd5f
Redouane Kachach <rkachach@redhat.com>
2023-03-03 10:39:09 +0100

    Revert "ci: fix MultiClusterDeploySuite CI"
    This reverts commit b4644289783a437241fea4eaac7906ea11a1ce85.
    
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>
    (cherry picked from commit 0c652d28e2809e4228dd161c739861cc440357d1)

--- pkg/operator/ceph/cluster/mgr/mgr_test.go
+++ pkg/operator/ceph/cluster/mgr/mgr_test.go
@@ -42,7 +42,6 @@ import (
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
@@ -59,8 +58,8 @@ func TestStartMgr(t *testing.T) {
 			return "{\"key\":\"mysecurekey\"}", nil
 		},
 	}
-	waitForPodsWithLabelToRun = func(ctx context.Context, clientset kubernetes.Interface, namespace, label string) error {
-		logger.Infof("simulated mgr deployment waiting for deployment")
+	waitForDeploymentToStart = func(ctx context.Context, clusterdContext *clusterd.Context, deployment *apps.Deployment) error {
+		logger.Infof("simulated mgr deployment starting")
 		return nil
 	}
 
