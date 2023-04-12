6969b459f7ceb90a14d046013bc22eaac8a4c807
avanthakkar <avanjohn@gmail.com>
2023-04-13 02:03:52 +0530

    core: disable exporter for Ceph v17.2.6
    The expected ceph version for exporter is changed temporarily because
    some regression was detected in Ceph version 17.2.6.
    Thus, disabling ceph-exporter for now until all the regression are fixed.
    
    Signed-off-by: avanthakkar <avanjohn@gmail.com>
    (cherry picked from commit 3e46522423466757ff885ffed83a6905682a7625)

--- pkg/operator/ceph/cluster/nodedaemon/exporter_test.go
+++ pkg/operator/ceph/cluster/nodedaemon/exporter_test.go
@@ -44,7 +44,7 @@ func TestCreateOrUpdateCephExporter(t *testing.T) {
 	}
 	cephCluster.Spec.Labels = cephv1.LabelsSpec{}
 	cephCluster.Spec.PriorityClassNames = cephv1.PriorityClassNamesSpec{}
-	cephVersion := &cephver.CephVersion{Major: 17, Minor: 2, Extra: 5}
+	cephVersion := &cephver.CephVersion{Major: 18, Minor: 0, Extra: 0}
 	ctx := context.TODO()
 	context := &clusterd.Context{
 		Clientset:     test.New(t, 1),
