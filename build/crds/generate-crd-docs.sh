2360f145e553dd0f03b1de1a719a61e2a5cdb1ed
parth-gr <paarora@redhat.com>
2023-06-15 13:13:03 +0530

    build: make crds should be able to build generated docs
    Currently, if there is any change in the API we need to separately
    run the make generate-crd-docs but with this change
    make crds will also update the generated docs
    
    Also improved the file locations
    
    Closes: https://github.com/rook/rook/issues/12310
    
    Signed-off-by: parth-gr <paarora@redhat.com>
    (cherry picked from commit 8175bc0218ef59332d111bfad3dbfacdcd007007)

--- build/crds/generate-crd-docs.sh
+++ build/crds/generate-crd-docs.sh
@@ -2,23 +2,24 @@
 
 SCRIPT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)
 GEN_CRD_API_REFERENCE_DOC_VERSION=v0.3.0
-SCRIPT_CHECK_DOCS_DIFF=$(git diff --name-only --diff-filter=M | grep -Ec '/types\.go$')
 SPECIFICATION_FILE=Documentation/CRDs/specification.md
 
 install_generator() {
-    go install github.com/ahmetb/gen-crd-api-reference-docs@${GEN_CRD_API_REFERENCE_DOC_VERSION}
+  go install github.com/ahmetb/gen-crd-api-reference-docs@${GEN_CRD_API_REFERENCE_DOC_VERSION}
 }
 
 run_gen() {
-  if [[  "$SCRIPT_CHECK_DOCS_DIFF" -gt 0 || "$1" = "--force" ]]; then
-    echo "differences found in types.go, rebuilding specification.md"
+  if [[ $SKIP_GEN_CRD_DOCS = true ]]; then
+    echo "SKIP_GEN_CRD_DOCS is set, skipping CRD docs generation"
+  else
+    echo "rebuilding specification.md"
     install_generator
-    gen-crd-api-reference-docs \
-        -config="${SCRIPT_ROOT}/crd-docs-config.json" \
-        -template-dir="${SCRIPT_ROOT}/Documentation/gen-crd-api-reference-docs/template" \
-        -api-dir="github.com/rook/rook/pkg/apis/ceph.rook.io" \
-        -out-file="${SCRIPT_ROOT}/$SPECIFICATION_FILE"
- fi
+    ${GOBIN}/gen-crd-api-reference-docs \
+      -config="${SCRIPT_ROOT}/build/crds/crd-docs-config.json" \
+      -template-dir="${SCRIPT_ROOT}/Documentation/gen-crd-api-reference-docs/template" \
+      -api-dir="github.com/rook/rook/pkg/apis/ceph.rook.io" \
+      -out-file="${SCRIPT_ROOT}/$SPECIFICATION_FILE"
+  fi
 }
 
-run_gen "$@"
+run_gen
