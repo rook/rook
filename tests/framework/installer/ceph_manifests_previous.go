6c16c0eb833d96eba86d3de48030aea070ce7678
travisn <tnielsen@redhat.com>
2023-11-15 16:11:14 -0700

    tests: upgrade from 1.12 to master
    The upgrade test should always upgrade from the previous
    minor release to the latest master. With 1.13 releasing
    soon, now we uprade from 1.12 to master, to confirm
    if there are any upgrade issues to 1.13.
    
    Signed-off-by: travisn <tnielsen@redhat.com>

--- tests/framework/installer/ceph_manifests_previous.go
+++ tests/framework/installer/ceph_manifests_previous.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	// The version from which the upgrade test will start
-	Version1_11 = "v1.11.7"
+	Version1_12 = "v1.12.7"
 )
 
 // CephManifestsPreviousVersion wraps rook yaml definitions
