93779d4e70d4e70d26cee5f880de0428cd575288
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-02-08 18:12:44 +0100

    try implicitTenants: true

--- tests/framework/installer/ceph_manifests.go
+++ tests/framework/installer/ceph_manifests.go
@@ -505,7 +505,7 @@ spec:
         - admin
         - member
         - service
-      implicitTenants: "swift"
+      implicitTenants: "true"
       revocationInterval: 1200
       serviceUserSecretName: usersecret
       tokenCacheSize: 1000
