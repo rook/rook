90bec3120367f71ce9a64b5a121702ed359133f0
travisn <tnielsen@redhat.com>
2024-02-14 16:46:22 -0700

    pool: skip crush rule update when not needed
    The crush rule will be updated when the failure domain changes.
    This update can be skipped when the expected crush rule is
    already configured. Otherwise, the log has a confusing message
    that makes it appear that the crush rule was updated
    when in fact it was not.
    
    Signed-off-by: travisn <tnielsen@redhat.com>

--- pkg/daemon/ceph/client/pool_test.go
+++ pkg/daemon/ceph/client/pool_test.go
@@ -249,6 +249,8 @@ func testCreateReplicaPool(t *testing.T, failureDomain, crushRoot, deviceClass, 
 func TestUpdateFailureDomain(t *testing.T) {
 	var newCrushRule string
 	currentFailureDomain := "rack"
+	currentDeviceClass := "default"
+	testCrushRuleName := "test_rule"
 	executor := &exectest.MockExecutor{}
 	context := &clusterd.Context{Executor: executor}
 	executor.MockExecuteCommandWithOutput = func(command string, args ...string) (string, error) {
@@ -256,7 +258,7 @@ func TestUpdateFailureDomain(t *testing.T) {
 		if args[1] == "pool" {
 			if args[2] == "get" {
 				assert.Equal(t, "mypool", args[3])
-				return `{"crush_rule": "test_rule"}`, nil
+				return fmt.Sprintf(`{"crush_rule": "%s"}`, testCrushRuleName), nil
 			}
 			if args[2] == "set" {
 				assert.Equal(t, "mypool", args[3])
@@ -267,7 +269,7 @@ func TestUpdateFailureDomain(t *testing.T) {
 		}
 		if args[1] == "crush" {
 			if args[2] == "rule" && args[3] == "dump" {
-				return fmt.Sprintf(`{"steps": [{"foo":"bar"},{"type":"%s"}]}`, currentFailureDomain), nil
+				return fmt.Sprintf(`{"steps": [{"item_name":"%s"},{"type":"%s"}]}`, currentDeviceClass, currentFailureDomain), nil
 			}
 			newCrushRule = "foo"
 			return "", nil
@@ -296,6 +298,7 @@ func TestUpdateFailureDomain(t *testing.T) {
 				Replicated:    cephv1.ReplicatedSpec{Size: 3},
 			},
 		}
+		testCrushRuleName = "mypool_rack"
 		clusterSpec := &cephv1.ClusterSpec{Storage: cephv1.StorageScopeSpec{}}
 		err := updatePoolCrushRule(context, AdminTestClusterInfo("mycluster"), clusterSpec, p)
 		assert.NoError(t, err)
