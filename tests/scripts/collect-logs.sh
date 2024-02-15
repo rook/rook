7fbb8f2379c876a17d2a5fd9855c65749b4b5431
Yuma Ogami <yuma-ogami@cybozu.co.jp>
2024-02-15 05:33:14 +0000

    DNM: test issue 13737
    reproduce 13737

--- tests/scripts/collect-logs.sh
+++ tests/scripts/collect-logs.sh
@@ -19,49 +19,50 @@ $CEPH_CMD report >"${LOG_DIR}"/ceph-report.txt
 
 NAMESPACES=("$CLUSTER_NAMESPACE")
 if [[ "$OPERATOR_NAMESPACE" != "$CLUSTER_NAMESPACE" ]]; then
-  NAMESPACES+=("$OPERATOR_NAMESPACE")
+        NAMESPACES+=("$OPERATOR_NAMESPACE")
 fi
 
 if [[ -n "${ADDITIONAL_NAMESPACE}" ]]; then
-  NAMESPACES+=("${ADDITIONAL_NAMESPACE}")
+        NAMESPACES+=("${ADDITIONAL_NAMESPACE}")
 fi
 
 for NAMESPACE in "${NAMESPACES[@]}"; do
-  # each namespace is a sub-directory for easier debugging
-  NS_DIR="${LOG_DIR}"/namespace-"${NAMESPACE}"
-  mkdir "${NS_DIR}"
+        # each namespace is a sub-directory for easier debugging
+        NS_DIR="${LOG_DIR}"/namespace-"${NAMESPACE}"
+        mkdir "${NS_DIR}"
 
-  # describe every one of the k8s resources in the namespace which rook commonly uses
-  for KIND in 'pod' 'deployment' 'job' 'daemonset' 'cm'; do
-    kubectl -n "$NAMESPACE" get "$KIND" -o wide >"${NS_DIR}"/"$KIND"-list.txt
-    for resource in $(kubectl -n "$NAMESPACE" get "$KIND" -o jsonpath='{.items[*].metadata.name}'); do
-      kubectl -n "$NAMESPACE" describe "$KIND" "$resource" >"${NS_DIR}"/"$KIND"-describe--"$resource".txt
+        # describe every one of the k8s resources in the namespace which rook commonly uses
+        for KIND in 'pod' 'deployment' 'job' 'daemonset' 'cm'; do
+                kubectl -n "$NAMESPACE" get "$KIND" -o wide >"${NS_DIR}"/"$KIND"-list.txt
+                for resource in $(kubectl -n "$NAMESPACE" get "$KIND" -o jsonpath='{.items[*].metadata.name}'); do
+                        kubectl -n "$NAMESPACE" describe "$KIND" "$resource" >"${NS_DIR}"/"$KIND"-describe--"$resource".txt
 
-      # collect logs for pods along the way
-      if [[ "$KIND" == 'pod' ]]; then
-        kubectl -n "$NAMESPACE" logs --all-containers "$resource" >"${NS_DIR}"/logs--"$resource".txt
-      fi
-    done
-  done
+                        # collect logs for pods along the way
+                        if [[ "$KIND" == 'pod' ]]; then
+                                kubectl -n "$NAMESPACE" logs --all-containers "$resource" >"${NS_DIR}"/logs--"$resource".txt
+                                kubectl -n "$NAMESPACE" logs -p --all-containers "$resource" >"${NS_DIR}"/logs--"$resource"-p.txt
+                        fi
+                done
+        done
 
-  # secret need `-oyaml` to read the content instead of `describe` since secrets `describe` will be encrypted.
-  # so keeping it in a different block.
-  for secret in $(kubectl -n "$NAMESPACE" get secrets -o jsonpath='{.items[*].metadata.name}'); do
-    kubectl -n "$NAMESPACE" get -o yaml secret "$secret" >"${NS_DIR}"/secret-describe--"$secret".txt
-  done
+        # secret need `-oyaml` to read the content instead of `describe` since secrets `describe` will be encrypted.
+        # so keeping it in a different block.
+        for secret in $(kubectl -n "$NAMESPACE" get secrets -o jsonpath='{.items[*].metadata.name}'); do
+                kubectl -n "$NAMESPACE" get -o yaml secret "$secret" >"${NS_DIR}"/secret-describe--"$secret".txt
+        done
 
-  # describe every one of the custom resources in the namespace since all should be rook-related and
-  # they aren't captured by 'kubectl get all'
-  for CRD in $(kubectl get crds -o jsonpath='{.items[*].metadata.name}'); do
-    for resource in $(kubectl -n "$NAMESPACE" get "$CRD" -o jsonpath='{.items[*].metadata.name}'); do
-      crd_main_type="${CRD%%.*}" # e.g., for cephclusters.ceph.rook.io, only use 'cephclusters'
-      kubectl -n "$NAMESPACE" get -o yaml "$CRD" "$resource" >"${NS_DIR}"/"$crd_main_type"-describe--"$resource".txt
-    done
-  done
+        # describe every one of the custom resources in the namespace since all should be rook-related and
+        # they aren't captured by 'kubectl get all'
+        for CRD in $(kubectl get crds -o jsonpath='{.items[*].metadata.name}'); do
+                for resource in $(kubectl -n "$NAMESPACE" get "$CRD" -o jsonpath='{.items[*].metadata.name}'); do
+                        crd_main_type="${CRD%%.*}" # e.g., for cephclusters.ceph.rook.io, only use 'cephclusters'
+                        kubectl -n "$NAMESPACE" get -o yaml "$CRD" "$resource" >"${NS_DIR}"/"$crd_main_type"-describe--"$resource".txt
+                done
+        done
 
-  # do simple 'get all' calls for resources we don't often want to look at
-  kubectl get all -n "$NAMESPACE" -o wide >"${NS_DIR}"/all-wide.txt
-  kubectl get all -n "$NAMESPACE" -o yaml >"${NS_DIR}"/all-yaml.txt
+        # do simple 'get all' calls for resources we don't often want to look at
+        kubectl get all -n "$NAMESPACE" -o wide >"${NS_DIR}"/all-wide.txt
+        kubectl get all -n "$NAMESPACE" -o yaml >"${NS_DIR}"/all-yaml.txt
 done
 
 sudo lsblk | sudo tee -a "${LOG_DIR}"/lsblk.txt
