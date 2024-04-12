30bdd43e27e60e0309d82afb1bd2d1647af1c77c
Liang Zheng <zhengliang0901@gmail.com>
2024-04-12 17:01:38 +0800

    core: fix typos in reconcile cluster disruption
    Signed-off-by: Liang Zheng <zhengliang0901@gmail.com>

--- pkg/operator/ceph/disruption/clusterdisruption/osd.go
+++ pkg/operator/ceph/disruption/clusterdisruption/osd.go
@@ -223,7 +223,7 @@ func (r *ReconcileClusterDisruption) reconcilePDBsForOSDs(
 	allFailureDomains,
 	osdDownFailureDomains []string,
 	activeNodeDrains bool,
-	pgHealhtyRegex string,
+	pgHealthyRegex string,
 ) (reconcile.Result, error) {
 	var osdDown bool
 	var drainingFailureDomain string
@@ -232,7 +232,7 @@ func (r *ReconcileClusterDisruption) reconcilePDBsForOSDs(
 		drainingFailureDomain = osdDownFailureDomains[0]
 	}
 
-	pgHealthMsg, pgClean, err := cephclient.IsClusterClean(r.context.ClusterdContext, clusterInfo, pgHealhtyRegex)
+	pgHealthMsg, pgClean, err := cephclient.IsClusterClean(r.context.ClusterdContext, clusterInfo, pgHealthyRegex)
 	if err != nil {
 		// If the error contains that message, this means the cluster is not up and running
 		// No monitors are present and thus no ceph configuration has been created
