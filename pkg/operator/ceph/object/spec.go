814d58bcf514b6c58c4153ac8f95eea125229724
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2024-08-26 15:41:40 +0530

    object: add rgw ops sidecar for op logs
    the rgw operations for s3 can now be accessible using sidecar
    rgw-ops-log availabe in json form that can be further filtered logging
    for observability, this will set the rgw_enable_ops_log setting
    
    Signed-off-by: Deepika Upadhyay <deepika.upadhyay@clyso.com>

--- pkg/operator/ceph/object/spec.go
+++ pkg/operator/ceph/object/spec.go
@@ -155,6 +155,25 @@ func (c *clusterConfig) makeRGWPodSpec(rgwConfig *rgwConfig) (v1.PodTemplateSpec
 		ServiceAccountName: serviceAccountName,
 	}
 
+	if c.store.Spec.RgwOpsLogEnabled != nil {
+		// If the rgw ops logs are enabled we add the side-car container
+		if *c.store.Spec.RgwOpsLogEnabled {
+			monStore := cephconfig.GetMonStore(c.context, c.clusterInfo)
+			if err := monStore.Set("client"+rgwConfig.DaemonID, "rgw_enable_ops_log", "true"); err != nil {
+				logger.Error(err, "failed to apply logging configuration for rgw ops log")
+			}
+			logger.Debug("rgw ops log is enabled now, creating side-car for it...")
+			shareProcessNamespace := true
+			podSpec.ShareProcessNamespace = &shareProcessNamespace
+			podSpec.Containers = append(podSpec.Containers, *controller.RgwOpsLogContainer(getDaemonName(rgwConfig), c.clusterInfo.Namespace, *c.clusterSpec, c.store.Spec.RgwOpsLog))
+		} else {
+			monStore := cephconfig.GetMonStore(c.context, c.clusterInfo)
+			if err := monStore.Set("client"+rgwConfig.DaemonID, "rgw_enable_ops_log", "false"); err != nil {
+				logger.Error(err, "failed to apply logging configuration for rgw ops log")
+			}
+		}
+	}
+
 	// If the log collector is enabled we add the side-car container
 	if c.clusterSpec.LogCollector.Enabled {
 		shareProcessNamespace := true
