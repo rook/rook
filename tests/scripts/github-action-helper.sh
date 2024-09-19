f6474445152822ff1635fe33cb84565b254031e0
subhamkrai <srai@redhat.com>
2024-09-19 15:03:59 +0530

    ci: fix ci permission issue with minikube start
    this commit upgrade the minikube, k8s, crictl versions
    in CI and also fix permission error in the github runner.
    
    Signed-off-by: subhamkrai <srai@redhat.com>

--- tests/scripts/github-action-helper.sh
+++ tests/scripts/github-action-helper.sh
@@ -707,8 +707,8 @@ function test_csi_nfs_workload {
 }
 
 function install_minikube_with_none_driver() {
-  CRICTL_VERSION="v1.30.0"
-  MINIKUBE_VERSION="v1.32.0"
+  CRICTL_VERSION="v1.31.1"
+  MINIKUBE_VERSION="v1.34.0"
 
   sudo apt update
   sudo apt install -y conntrack socat
@@ -716,16 +716,16 @@ function install_minikube_with_none_driver() {
   sudo dpkg -i minikube_latest_amd64.deb
   rm -f minikube_latest_amd64.deb
 
-  curl -LO https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.9/cri-dockerd_0.3.9.3-0.ubuntu-focal_amd64.deb
-  sudo dpkg -i cri-dockerd_0.3.9.3-0.ubuntu-focal_amd64.deb
-  rm -f cri-dockerd_0.3.9.3-0.ubuntu-focal_amd64.deb
+  curl -LO https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.15/cri-dockerd_0.3.15.3-0.ubuntu-focal_amd64.deb
+  sudo dpkg -i cri-dockerd_0.3.15.3-0.ubuntu-focal_amd64.deb
+  rm -f cri-dockerd_0.3.15.3-0.ubuntu-focal_amd64.deb
 
   wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$CRICTL_VERSION/crictl-$CRICTL_VERSION-linux-amd64.tar.gz
   sudo tar zxvf crictl-$CRICTL_VERSION-linux-amd64.tar.gz -C /usr/local/bin
   rm -f crictl-$CRICTL_VERSION-linux-amd64.tar.gz
   sudo sysctl fs.protected_regular=0
 
-  CNI_PLUGIN_VERSION="v1.4.0"
+  CNI_PLUGIN_VERSION="v1.5.1"
   CNI_PLUGIN_TAR="cni-plugins-linux-amd64-$CNI_PLUGIN_VERSION.tgz" # change arch if not on amd64
   CNI_PLUGIN_INSTALL_DIR="/opt/cni/bin"
 
@@ -735,7 +735,7 @@ function install_minikube_with_none_driver() {
   rm "$CNI_PLUGIN_TAR"
 
   export MINIKUBE_HOME=$HOME CHANGE_MINIKUBE_NONE_USER=true KUBECONFIG=$HOME/.kube/config
-  sudo -E minikube start --kubernetes-version="$1" --driver=none --memory 6g --cpus=2 --addons ingress --cni=calico
+  minikube start --kubernetes-version="$1" --driver=none --memory 6g --cpus=2 --addons ingress --cni=calico
 }
 
 FUNCTION="$1"
