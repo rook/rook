71d5dc812936b9afc59a9873c1e726d36affb2ce
subhamkrai <srai@redhat.com>
2024-09-25 13:50:38 +0530

    csi: create csi-operator resources in op. ns
    Signed-off-by: subhamkrai <srai@redhat.com>

--- tests/scripts/github-action-helper.sh
+++ tests/scripts/github-action-helper.sh
@@ -293,6 +293,7 @@ function deploy_cluster() {
   cd "${REPO_DIR}/deploy/examples"
 
   deploy_manifest_with_local_build operator.yaml
+  kubectl create -f csi-operator.yaml
 
   if [ $# == 0 ]; then
     sed -i "s|#deviceFilter:|deviceFilter: $(block_dev_basename)|g" cluster-test.yaml
@@ -475,6 +476,7 @@ function deploy_first_rook_cluster() {
   cd "${REPO_DIR}/deploy/examples"
 
   deploy_manifest_with_local_build operator.yaml
+  deploy_manifest_with_local_build csi-operator.yaml
   yq w -i -d0 cluster-test.yaml spec.dashboard.enabled false
   yq w -i -d0 cluster-test.yaml spec.storage.useAllDevices false
   yq w -i -d0 cluster-test.yaml spec.storage.deviceFilter "${DEVICE_NAME}"1
