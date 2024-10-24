22850b7d2a5a703e7a5c38541006d8623e16f02d
subhamkrai <srai@redhat.com>
2024-10-24 11:37:31 +0530

    ci: test multus ip addressRanges
    Signed-off-by: subhamkrai <srai@redhat.com>

--- tests/scripts/github-action-helper.sh
+++ tests/scripts/github-action-helper.sh
@@ -273,8 +273,8 @@ function deploy_toolbox() {
 }
 
 function replace_ceph_image() {
-  local file="$1"  # parameter 1: the file in which to replace the ceph image
-  local ceph_image="${2?ceph_image is required}"  # parameter 2: the new ceph image to use
+  local file="$1"                                # parameter 1: the file in which to replace the ceph image
+  local ceph_image="${2?ceph_image is required}" # parameter 2: the new ceph image to use
 
   # check for ceph_image being an empty string
   if [ -z "$ceph_image" ]; then
@@ -766,7 +766,7 @@ function test_object_separate_pools() {
     done
     if [[ "$found" == false ]]; then
       echo "Live pool $l is not an expected pool"
-      errors=$((errors+1))
+      errors=$((errors + 1))
     fi
   done
 
