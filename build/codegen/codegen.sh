5ce64f601cd3bf57f6e286bb6f0f404bc475f2ba
Artem Torubarov <artem.torubarov@clyso.com>
2024-11-08 16:20:51 +0100

    build: validate codegen path
    Signed-off-by: Artem Torubarov <artem.torubarov@clyso.com>

--- build/codegen/codegen.sh
+++ build/codegen/codegen.sh
@@ -14,9 +14,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# outputs absolute path for given relative path.
+function toAbsolutePath(){
+  echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+}
+
+
 GROUP_VERSIONS="ceph.rook.io:v1"
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+output_base="$(dirname "${BASH_SOURCE[0]}")/../../../../.."
+output_pkg="github.com/rook/rook/pkg/client"
+apis_pkg="github.com/rook/rook/pkg/apis"
+
+# check that output directory for generated packages already exists.
+if [ ! -d "$output_base/$apis_pkg" ]; then
+  echo "apis pkg $(toAbsolutePath $output_base/$apis_pkg) does not exist."
+  echo "please make sure that project located in 'github.com/rook/rook' directory"
+  exit 1
+fi
+
+echo "Generate code for packages: 
+  - $(toAbsolutePath $output_base/$output_pkg)
+  - $(toAbsolutePath $output_base/$apis_pkg)"
 
 # CODE GENERATION
 # we run deepcopy and client,lister,informer generations separately so we can use the flag "--plural-exceptions"
@@ -25,18 +46,18 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # run code deepcopy generation
 bash ${CODE_GENERATOR}/generate-groups.sh \
     deepcopy \
-    github.com/rook/rook/pkg/client \
-    github.com/rook/rook/pkg/apis \
+    $output_pkg \
+    $apis_pkg \
     "${GROUP_VERSIONS}" \
-    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../../.." \
+    --output-base $output_base \
     --go-header-file "${scriptdir}/boilerplate.go.txt"
 
 # run code client,lister,informer generation
 bash ${CODE_GENERATOR}/generate-groups.sh \
     client,lister,informer \
-    github.com/rook/rook/pkg/client \
-    github.com/rook/rook/pkg/apis \
+    $output_pkg \
+    $apis_pkg \
     "${GROUP_VERSIONS}" \
-    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../../.." \
+    --output-base $output_base \
     --go-header-file "${scriptdir}/boilerplate.go.txt" \
     --plural-exceptions "CephNFS:CephNFSes" \
