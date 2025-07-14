574af13bfaf86870cf78b0bdb973eabc27a89bd5
subhamkrai <srai@redhat.com>
2025-07-14 22:16:16 +0530

    test: add `pathType` with ingress dashboard host
    Recentally, helm related ci started failing due to
    ```
    testutil: failed to install chart. result=. walk.go:75: found symbolic link in path: /home/runner/work/rook/rook/deploy/charts/rook-ceph-cluster/charts/library resolves to /home/runner/work/rook/rook/deploy/charts/library. Contents of linked file included and used
    I0714 09:44:18.963620   18291 warnings.go:110] "Warning: path /ceph-dashboard(/|$)(.*) cannot be used with pathType Prefix"
    Error: INSTALLATION FAILED: 1 error occurred:
    	* admission webhook "validate.nginx.ingress.kubernetes.io" denied the request: ingress contains invalid paths: path /ceph-dashboard(/|$)(.*) cannot be used with pathType Prefix, err=failed to run helm command on args [install --create-namespace rook-ceph-cluster /home/runner/work/rook/rook/deploy/charts/rook-ceph-cluster --namespace helm-ns -f values-test.yaml] : . walk.go:75: found symbolic link in path: /home/runner/work/rook/rook/deploy/charts/rook-ceph-cluster/charts/library resolves to /home/runner/work/rook/rook/deploy/charts/library. Contents of linked file included and used
    I0714 09:44:18.963620   18291 warnings.go:110] "Warning: path /ceph-dashboard(/|$)(.*) cannot be used with pathType Prefix"
    Error: INSTALLATION FAILED: 1 error occurred:
    ```
    as default `pathType` is `Prefix`, we need to change it to
    `pathType:ImplementationSpecific`.
    
    Signed-off-by: subhamkrai <srai@redhat.com>
    (cherry picked from commit 8bd4b38399b9aa24a0e3b9c68c021ddde4cdd80c)

--- tests/framework/installer/ceph_helm_installer.go
+++ tests/framework/installer/ceph_helm_installer.go
@@ -130,8 +130,9 @@ func (h *CephInstaller) configureRookCephClusterViaHelm(upgrade bool) error {
 				"nginx.ingress.kubernetes.io/rewrite-target": "/ceph-dashboard/$2",
 			},
 			"host": map[string]interface{}{
-				"name": "localhost",
-				"path": "/ceph-dashboard(/|$)(.*)",
+				"name":     "localhost",
+				"path":     "/ceph-dashboard(/|$)(.*)",
+				"pathType": "ImplementationSpecific",
 			},
 		},
 	}
