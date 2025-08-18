7b5d4768879fc8053e804826f4b572a1f1941ca5
parth-gr <partharora1010@gmail.com>
2025-08-18 15:31:39 +0530

    csi: fix the prior key count for csi key rotation
    fix the prior key count status for the csi
    key rotation
    
    Signed-off-by: parth-gr <partharora1010@gmail.com>

--- pkg/operator/ceph/csi/secrets_test.go
+++ pkg/operator/ceph/csi/secrets_test.go
@@ -522,3 +522,22 @@ func TestParseCsiClient(t *testing.T) {
 		})
 	}
 }
+
+func TestGetPriorKeyCount(t *testing.T) {
+	tests := []struct {
+		name     string
+		keyCount int
+		want     int
+	}{
+		{"keyGen 0", 0, 0},
+		{"keyGen 1", 1, 0},
+		{"keyGen 2", 2, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getPriorKeyCount(tt.keyCount)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
