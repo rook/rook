ac753dc818bac14df476596298c361e4b920f80d
Elias Carter <elias@dropbox.com>
2025-09-17 15:42:53 -0700

    osd: cleanup orphaned osd provision jobs
    Currently we have logic which will cleanup any dangling osd provision status configmaps,
    but we do not have any logic which will cleanup dangling osd provision jobs.
    
    In a running cluster, I noticed that a node which was provisioning was removed from k8s
    and the following log was emitted by the rook operator:
    ```
    op-osd: cleaning up dangling OSD prepare status configmap "rook-ceph-osd-xxx-status"
    ```
    
    But the osd provisioning job was still trying to run on the removed node:
    ```
    rook-ceph-osd-prepare-xxx         Running    0/1           4d22h      4d22h
    ```
    
    What this PR does is remove dangling osd provision jobs in addition to dangling
    provisioning status configmaps. I added a test for this cleanup as there wasn't
    a pre-existing test.
    
    Signed-off-by: Elias Carter <elias@dropbox.com>

--- pkg/operator/ceph/cluster/osd/osd_test.go
+++ pkg/operator/ceph/cluster/osd/osd_test.go
@@ -37,6 +37,7 @@ import (
 	exectest "github.com/rook/rook/pkg/util/exec/test"
 	"github.com/stretchr/testify/assert"
 	apps "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -520,6 +521,93 @@ func TestAddNodeFailure(t *testing.T) {
 	// verify orchestration failed (because the operator failed to create a job)
 	assert.True(t, startCompleted)
 	assert.NotNil(t, startErr)
+}
+
+func TestOSDProvisionCleanup(t *testing.T) {
+	// Test that we cleanup orphaned provision status configmaps and provision jobs during reconciliation.
+	nodeName := "missingNode"
+	namespace := "ns"
+
+	clientset := fake.NewSimpleClientset()
+
+	// Create orphaned status configmap
+	cmName := statusConfigMapName(nodeName)
+	cmLabels := statusConfigMapLabels(nodeName)
+	_, err := clientset.CoreV1().ConfigMaps(namespace).Create(context.TODO(), &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   cmName,
+			Labels: cmLabels,
+		},
+	}, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	// Create orphaned provision job
+	jobName := provisionJobName(nodeName)
+	jobLabels := provisionJobLabels(namespace)
+	_, err = clientset.BatchV1().Jobs(namespace).Create(context.TODO(), &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   jobName,
+			Labels: jobLabels,
+		},
+	}, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	t.Setenv(k8sutil.PodNamespaceEnvVar, "rook-system")
+
+	cephCluster := &cephv1.CephCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testing",
+			Namespace: namespace,
+		},
+	}
+	// Objects to track in the fake client.
+	object := []runtime.Object{
+		cephCluster,
+	}
+	s := scheme.Scheme
+	// Create a fake client to mock API calls.
+	client := clientfake.NewClientBuilder().WithScheme(s).WithRuntimeObjects(object...).Build()
+	clusterInfo := &cephclient.ClusterInfo{
+		Namespace:   namespace,
+		CephVersion: cephver.Squid,
+		Context:     context.TODO(),
+	}
+	clusterInfo.SetName("testcluster")
+	clusterInfo.OwnerInfo = cephclient.NewMinimumOwnerInfo(t)
+	executor := &exectest.MockExecutor{
+		MockExecuteCommandWithOutput: func(command string, args ...string) (string, error) {
+			logger.Infof("ExecuteCommandWithOutput: %s %v", command, args)
+			if args[1] == "crush" && args[2] == "class" && args[3] == "ls" {
+				// Mock executor for OSD crush class list command, returning ssd as available device class
+				return `["ssd"]`, nil
+			}
+			if args[0] == "osd" && args[1] == "df" {
+				return osdDFResults, nil
+			}
+			return "", nil
+		},
+	}
+	ctx := &clusterd.Context{Clientset: clientset, Client: client, ConfigDir: "/var/lib/rook", Executor: executor}
+	spec := cephv1.ClusterSpec{
+		DataDirHostPath: ctx.ConfigDir,
+		Storage: cephv1.StorageScopeSpec{
+			Nodes: []cephv1.Node{},
+		},
+	}
+
+	c := New(ctx, clusterInfo, spec, "myversion")
+
+	// run the reconciliation
+	err = c.Start()
+	assert.Nil(t, err)
+
+	// validate that orphaned status configmap was deleted
+	_, err = clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), cmName, metav1.GetOptions{})
+	assert.True(t, k8serrors.IsNotFound(err), "Orphaned status configmap was not deleted.")
+
+	// validate that orphaned provision job was deleted
+	_, err = clientset.BatchV1().Jobs(namespace).Get(context.TODO(), jobName, metav1.GetOptions{})
+	assert.True(t, k8serrors.IsNotFound(err), "Orphaned provision job was not deleted.")
 }
 
 func TestGetPVCHostName(t *testing.T) {
