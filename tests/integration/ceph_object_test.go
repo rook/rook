3267048186b7d911244e8a19013295d96cec9c90
Joshua Hoblitt <josh@hoblitt.com>
2026-06-29 18:00:00 +0000

    test: cover CephObjectStoreUser.Spec.DefaultPlacement in the object suite
    Add a non-default "bar" pool placement to the shared store's zone, then a
    user/placement package that sets Spec.DefaultPlacement to "bar" on a
    CephObjectStoreUser and verifies through the rgw admin API that the user's
    default_placement is the override, then changes it to "default". Wire the
    package into the suite dispatcher and allow its namespace on the shared store.
    
    Signed-off-by: Joshua Hoblitt <josh@hoblitt.com>

--- tests/integration/ceph_object_test.go
+++ tests/integration/ceph_object_test.go
@@ -54,6 +54,7 @@ import (
 	usercaps "github.com/rook/rook/tests/integration/object/user/caps"
 	userkeys "github.com/rook/rook/tests/integration/object/user/keys"
 	useropmask "github.com/rook/rook/tests/integration/object/user/opmask"
+	userplacement "github.com/rook/rook/tests/integration/object/user/placement"
 	"github.com/rook/rook/tests/integration/object/util/sharedstore"
 )
 
@@ -223,6 +224,7 @@ func runObjectE2ETest(helper *clients.TestClient, k8sh *utils.K8sHelper, install
 		topickafka.Namespace,
 		useropmask.Namespace,
 		usercaps.Namespace,
+		userplacement.Namespace,
 		cosi.Namespace,
 		notification.Namespace,
 	)
@@ -236,6 +238,7 @@ func runObjectE2ETest(helper *clients.TestClient, k8sh *utils.K8sHelper, install
 	topickafka.TestBucketTopicKafka(s.T(), k8sh, sharedObjectStore)
 	useropmask.TestObjectStoreUserOpMask(s.T(), k8sh, sharedObjectStore)
 	usercaps.TestObjectStoreUserCaps(s.T(), k8sh, sharedObjectStore)
+	userplacement.TestObjectStoreUserDefaultPlacement(s.T(), k8sh, sharedObjectStore)
 	// the ceph-cosi driver cannot reach a TLS object store endpoint, so this
 	// suite skips itself in the TLS pass
 	cosi.TestCephCOSIDriver(s.T(), k8sh, sharedObjectStore)
