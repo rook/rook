3e5b4f4f59da86406b11a448427cbee1ec7dd572
Joshua Hoblitt <josh@hoblitt.com>
2026-07-21 15:14:08 -0700

    osd: extract the osd daemon start script to a file
    Move the cephOSDStart shell script from an embedded Go string constant
    into a standalone cephosd-start.sh embedded with go:embed, so it can be
    linted by shellcheck and read as a real shell script. The script takes
    no parameters, so behavior is unchanged.
    
    Signed-off-by: Joshua Hoblitt <josh@hoblitt.com>

--- pkg/operator/ceph/cluster/osd/spec.go
+++ pkg/operator/ceph/cluster/osd/spec.go
@@ -18,6 +18,7 @@ limitations under the License.
 package osd
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
 	"path"
@@ -71,34 +72,10 @@ const (
 	osdPortv2             = 6800
 )
 
-const (
-	cephOSDStart = `
-set -o nounset # fail if variables are unset
-child_pid=""
-sigterm_received=false
-function sigterm() {
-	echo "SIGTERM received"
-	sigterm_received=true
-	kill -TERM "$child_pid"
-}
-trap sigterm SIGTERM
-"${@}" &
-# un-fixable race condition: if receive sigterm here, it won't be sent to child process
-child_pid="$!"
-wait "$child_pid" # wait returns the same return code of child process when called with argument
-wait "$child_pid" # first wait returns immediately upon SIGTERM, so wait again for child to actually stop; this is a noop if child exited normally
-ceph_osd_rc=$?
-if [ $ceph_osd_rc -eq 0 ] && ! $sigterm_received; then
-	touch /tmp/osd-sleep
-	echo "OSD daemon exited with code 0, possibly due to OSD flapping. The OSD pod will sleep for $ROOK_OSD_RESTART_INTERVAL hours. Restart the pod manually once the flapping issue is fixed"
-	sleep "$ROOK_OSD_RESTART_INTERVAL"h &
-	child_pid="$!"
-	wait "$child_pid"
-	wait "$child_pid" # wait again for sleep to stop
-fi
-exit $ceph_osd_rc
-`
+//go:embed cephosd-start.sh
+var cephOSDStart string
 
+const (
 	activateOSDOnNodeCode = `
 set -o errexit
 set -o pipefail
@@ -1519,7 +1496,8 @@ func (c *Cluster) getCephxKeyUpdateInitContainer(osdID string, osdProps osdPrope
 	} else {
 		// assumption: activate OSD volume will already be added to pod spec for non-PVC OSDs at the
 		// correct location for mounting to /var/lib/ceph/osd/ceph-<id>
-		volMounts = append(volMounts,
+		volMounts = append(
+			volMounts,
 			v1.VolumeMount{Name: activateOSDVolumeName, MountPath: osdConfigMountPath},
 		)
 	}
