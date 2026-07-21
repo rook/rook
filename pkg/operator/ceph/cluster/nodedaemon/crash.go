cf4694ff690ddf35e2f36cd8af319a5246fb1e01
Laurent Goderre <laurent.goderre@docker.com>
2026-07-21 10:24:32 -0400

    operator: explicitly set root user for chown initContainer
    Signed-off-by: Laurent Goderre <laurent.goderre@docker.com>

--- pkg/operator/ceph/cluster/nodedaemon/crash.go
+++ pkg/operator/ceph/cluster/nodedaemon/crash.go
@@ -147,7 +147,7 @@ func getCrashDirInitContainer(cephCluster cephv1.CephCluster) corev1.Container {
 		},
 		Image:           cephCluster.Spec.CephVersion.Image,
 		ImagePullPolicy: controller.GetContainerImagePullPolicy(cephCluster.Spec.CephVersion.ImagePullPolicy),
-		SecurityContext: controller.DefaultContainerSecurityContext(),
+		SecurityContext: controller.RootContainerSecurityContext(),
 		Resources:       cephv1.GetCrashCollectorResources(cephCluster.Spec.Resources),
 		VolumeMounts:    controller.DaemonVolumeMounts(dataPathMap, "", cephCluster.Spec.DataDirHostPath),
 	}
@@ -163,7 +163,7 @@ func getCrashChownInitContainer(cephCluster cephv1.CephCluster) corev1.Container
 		controller.GetContainerImagePullPolicy(cephCluster.Spec.CephVersion.ImagePullPolicy),
 		controller.DaemonVolumeMounts(dataPathMap, "", cephCluster.Spec.DataDirHostPath),
 		cephv1.GetCrashCollectorResources(cephCluster.Spec.Resources),
-		controller.DefaultContainerSecurityContext(),
+		controller.RootContainerSecurityContext(),
 		"",
 	)
 }
